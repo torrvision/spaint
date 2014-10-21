d3b15c8a9de5b4c535753ee64e42242c30ccb2d0
Stuart Golodetz <sgolodetz@gxstudios.net>
2014-10-21 11:32:17 +0100

    Add input handling.

--- apps/spaintrunner/Application.cpp
+++ apps/spaintrunner/Application.cpp
@@ -5,6 +5,7 @@
 #include "Application.h"
 
 #include <spaint/ogl/WrappedGL.h>
+using namespace spaint;
 
 //#################### CONSTRUCTORS ####################
 
@@ -38,12 +39,58 @@ void Application::run()
 {
   for(;;)
   {
-    if(!process_events()) return;
+    if(!process_events() || m_inputState.key_down(SDLK_ESCAPE)) return;
     render();
   }
 }
 
 //#################### PRIVATE MEMBER FUNCTIONS ####################
+
+void Application::handle_key_down(const SDL_Keysym& keysym)
+{
+  m_inputState.press_key(keysym.sym);
+}
+
+void Application::handle_key_up(const SDL_Keysym& keysym)
+{
+  m_inputState.release_key(keysym.sym);
+}
+
+void Application::handle_mousebutton_down(const SDL_MouseButtonEvent& e)
+{
+  switch(e.button)
+  {
+    case SDL_BUTTON_LEFT:
+      m_inputState.press_mouse_button(MOUSE_BUTTON_LEFT, e.x, e.y);
+      break;
+    case SDL_BUTTON_MIDDLE:
+      m_inputState.press_mouse_button(MOUSE_BUTTON_MIDDLE, e.x, e.y);
+      break;
+    case SDL_BUTTON_RIGHT:
+      m_inputState.press_mouse_button(MOUSE_BUTTON_RIGHT, e.x, e.y);
+      break;
+    default:
+      break;
+  }
+}
+
+void Application::handle_mousebutton_up(const SDL_MouseButtonEvent& e)
+{
+  switch(e.button)
+  {
+    case SDL_BUTTON_LEFT:
+      m_inputState.release_mouse_button(MOUSE_BUTTON_LEFT);
+      break;
+    case SDL_BUTTON_MIDDLE:
+      m_inputState.release_mouse_button(MOUSE_BUTTON_MIDDLE);
+      break;
+    case SDL_BUTTON_RIGHT:
+      m_inputState.release_mouse_button(MOUSE_BUTTON_RIGHT);
+      break;
+    default:
+      break;
+  }
+}
 
 bool Application::process_events()
 {
@@ -53,6 +100,22 @@ bool Application::process_events()
     switch(event.type)
     {
       case SDL_KEYDOWN:
+        handle_key_down(event.key.keysym);
+        break;
+      case SDL_KEYUP:
+        handle_key_up(event.key.keysym);
+        break;
+      case SDL_MOUSEBUTTONDOWN:
+        handle_mousebutton_down(event.button);
+        break;
+      case SDL_MOUSEBUTTONUP:
+        handle_mousebutton_up(event.button);
+        break;
+      case SDL_MOUSEMOTION:
+        m_inputState.set_mouse_position(event.motion.x, event.motion.y);
+        m_inputState.set_mouse_motion(event.motion.xrel, event.motion.yrel);
+        break;
+      case SDL_QUIT:
         return false;
       default:
         break;
