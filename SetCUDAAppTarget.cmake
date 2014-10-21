4ba1be61b853c250ec543b58284ffcbff1edc602
Stuart Golodetz <sgolodetz@gxstudios.net>
2014-10-21 13:12:12 +0100

    Make CUDA support optional.

--- SetCUDAAppTarget.cmake
+++ SetCUDAAppTarget.cmake
@@ -5,7 +5,13 @@
 SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin/apps/${targetname})
 SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${PROJECT_BINARY_DIR}/bin/apps/${targetname})
 SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${PROJECT_BINARY_DIR}/bin/apps/${targetname})
-CUDA_ADD_EXECUTABLE(${targetname} ${sources} ${headers} ${templates} OPTIONS --generate-code arch=compute_20,code=sm_20 --generate-code arch=compute_30,code=sm_30)
+
+IF(WITH_CUDA)
+  CUDA_ADD_EXECUTABLE(${targetname} ${sources} ${headers} ${templates} OPTIONS --generate-code arch=compute_20,code=sm_20 --generate-code arch=compute_30,code=sm_30)
+ELSE()
+  ADD_EXECUTABLE(${targetname} ${sources} ${headers} ${templates})
+ENDIF()
+
 INCLUDE(${PROJECT_SOURCE_DIR}/VCLibraryHack.cmake)
 
 IF(MSVC_IDE)
--- UseCUDA.cmake
+++ UseCUDA.cmake
@@ -2,14 +2,20 @@
 # UseCUDA.cmake #
 #################
 
-FIND_PACKAGE(CUDA)
+OPTION(WITH_CUDA "Build with CUDA support?" ON)
 
-SET(CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" FORCE)
+IF(WITH_CUDA)
+  FIND_PACKAGE(CUDA)
 
-# If on Mac OS X 10.9 (Mavericks), make sure everything compiles and links using the correct C++ Standard Library.
-IF(${CMAKE_SYSTEM} MATCHES "Darwin-13.")
-	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libstdc++")
-	SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libstdc++")
-	SET(CUDA_HOST_COMPILER /usr/bin/clang)
-	SET(CUDA_NVCC_FLAGS -Xcompiler -stdlib=libstdc++; -Xlinker -stdlib=libstdc++; ${CUDA_NVCC_FLAGS})
+  SET(CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" FORCE)
+
+  # If on Mac OS X 10.9 (Mavericks), make sure everything compiles and links using the correct C++ Standard Library.
+  IF(${CMAKE_SYSTEM} MATCHES "Darwin-13.")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libstdc++")
+    SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libstdc++")
+    SET(CUDA_HOST_COMPILER /usr/bin/clang)
+    SET(CUDA_NVCC_FLAGS -Xcompiler -stdlib=libstdc++; -Xlinker -stdlib=libstdc++; ${CUDA_NVCC_FLAGS})
+  ENDIF()
+
+  ADD_DEFINITIONS(-DWITH_CUDA)
 ENDIF()
