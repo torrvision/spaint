924895328d9626ef06e961f78526d272aaafb85d
Stuart Golodetz <sgolodetz@gxstudios.net>
2014-10-25 09:07:22 +0100

    Update the OVR test to repeatedly get the pose of the HMD and print it out.

--- tests/test-ovr/main.cpp
+++ tests/test-ovr/main.cpp
@@ -1,23 +1,57 @@
 #include <iostream>
 
 #include <OVR.h>
+#include <../Src/Kernel/OVR_Math.h>
+using namespace OVR;
+
+void quit(ovrHmd hmd)
+{
+  ovrHmd_Destroy(hmd);
+  ovr_Shutdown();
+  exit(0);
+}
 
 int main()
 {
   ovr_Initialize();
   ovrHmd hmd = ovrHmd_Create(0);
 
-  if(hmd)
+  if(!hmd)
   {
-    // Get the resolution of the head-mounted display.
-    ovrSizei resolution = hmd->Resolution;
-
-    std::cout << resolution.w << 'x' << resolution.h << '\n';
+    std::cout << "Could not find the Oculus!\n";
+    quit(hmd);
   }
-  else std::cout << "Could not find the Oculus!\n";
 
-  ovrHmd_Destroy(hmd);
-  ovr_Shutdown();
+  // Get the resolution of the head-mounted display.
+  ovrSizei resolution = hmd->Resolution;
+  std::cout << "HMD Resolution: " << resolution.w << 'x' << resolution.h << '\n';
+
+  // Configure and start the sensor that provides the Rift's pose and motion.
+  std::cout << "Configuring tracking...";
+  std::cout.flush();
+  ovrHmd_ConfigureTracking(hmd, ovrTrackingCap_Orientation | ovrTrackingCap_MagYawCorrection | ovrTrackingCap_Position, 0);
+  std::cout << "done" << std::endl;
+
+  // Recentre the pose.
+  ovrHmd_RecenterPose(hmd);
+
+  for(;;)
+  {
+    // Query the HMD for the current tracking state.
+    ovrTrackingState ts = ovrHmd_GetTrackingState(hmd, ovr_GetTimeInSeconds());
+
+    // If we were able to get the orientation of the HMD, print out the Euler angles.
+    if(ts.StatusFlags & ovrStatus_OrientationTracked)
+    {
+      Posef pose = ts.HeadPose.ThePose;
+      float yaw, eyePitch, eyeRoll;
+      pose.Rotation.GetEulerAngles<Axis_Y,Axis_X,Axis_Z>(&yaw, &eyePitch, &eyeRoll);
+
+      std::cout << yaw << ' ' << eyePitch << ' ' << eyeRoll << '\n';
+    }
+  }
+
+  quit(hmd);
 
   return 0;
 }
