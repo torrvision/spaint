6286d039e3ba3db6262585a8a432b97c0b3ed08b
mikesapi <mikesapi.code.email@gmail.com>
2014-10-31 16:56:03 +0000

    added new hello-world style sdl-test, and initial forest directory

---
+++ modules/forest/src/ForestEngine.cpp
@@ -0,0 +1,12 @@
+#include "ForestEngine.h"
+
+#include <iostream>
+
+namespace forest{
+  
+ForestEngine::ForestEngine(int a)
+  : m_a(a){
+  std::cout << "Hi I'm ForestEngine and my value is:" << m_a << std::endl;
+}
+
+}
---
+++ tests/test-forest/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#include "forest/ForestEngine.h"
+
+
+int main()
+{
+  forest::ForestEngine my_forest(5);
+  
+  return 0;
+}
