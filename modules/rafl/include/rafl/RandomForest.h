be0e8b5c42b2b30232c010723cce6d318f0ec117
Stuart Golodetz <sgolodetz@gxstudios.net>
2014-11-03 18:30:13 +0000

    Continue working on the initial design.

--- modules/rafl/include/rafl/RandomForest.h
+++ modules/rafl/include/rafl/RandomForest.h
@@ -8,8 +8,38 @@
 #include <vector>
 
 #include <tvgutil/SharedPtr.h>
+#include "base/Example.h"
 
 namespace rafl {
+
+template <typename Label>
+class ExampleReservoir
+{
+    //#################### PRIVATE VARIABLES ####################
+private:
+  std::vector<Example> m_examples;
+  size_t m_maxSize;
+
+
+  //#################### PUBLIC MEMBER FUNCTIONS ####################
+public: 
+  explicit ExampleReservoir(size_t maxSize)
+  : m_maxSize(maxSize)
+  {}
+
+    //#################### PUBLIC MEMBER FUNCTIONS ####################
+public:
+  void add_example(const Example& example)
+  {
+  }
+};
+
+
+template <typename Label>
+class Node
+{
+
+};
 
 /**
  * \brief An instance of an instantiation of this class template represents a decision tree suitable for use within a random forest.
@@ -17,6 +47,7 @@ namespace rafl {
 template <typename Label>
 class DecisionTree
 {
+private:
 
   //#################### PUBLIC MEMBER FUNCTIONS ####################
 public: 
