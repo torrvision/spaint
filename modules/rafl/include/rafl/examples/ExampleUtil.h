2ba6c028d8852e2e42fb3877a76dec8730c913e7
mikesapi <mikesapi.code.email@gmail.com>
2014-11-25 11:41:43 +0000

    Cleaned up ExampleUtil

--- modules/rafl/include/rafl/examples/ExampleUtil.h
+++ modules/rafl/include/rafl/examples/ExampleUtil.h
@@ -34,13 +34,6 @@ public:
     (*d)[1] = y;
     return d;
   }
-  static Descriptor_Ptr make_2d_descriptor(float x, float y)
-  {
-    Descriptor_Ptr d(new Descriptor(2));
-    (*d)[0] = x;
-    (*d)[1] = y;
-    return d;
-  }
   
   /**
    * \brief Calculates the entropy of the label distribution of a set of examples.
@@ -98,6 +91,9 @@ public:
     return ProbabilityMassFunction<Label>(make_histogram(examples));
   }
   
+  };
+
+
   /**
    * \brief Creates a vector of examples with the same number of examples per class
    * 
@@ -105,48 +101,6 @@ public:
    * \param numberOfSamplesPerClass   The number of samples to create per class
    * \return                          The set of generated examples
    */
-  template <typename Label>
-  static std::vector<boost::shared_ptr<const Example<Label> > > unit_circle_example_generator(const std::set<Label>& labelSet, size_t numberOfSamplesPerClass)
-  {
-    
-    typedef boost::shared_ptr<const Example<Label> > Example_CPtr;
-    std::vector<Example_CPtr> exampleSet;
-
-    const double pi = 3.14f;
-
-    std::vector<Label> labelSetVector(labelSet.begin(), labelSet.end());
-
-    //Defines the angle with which to rotate the unit vector [d1 d2]' 
-    const float rotation_per_class = 2.f*pi/numberOfSamplesPerClass;
-    
-    //typename std::set<Label>::const_iterator set_iterator = labelSet.begin();
-    const float d1 = 0.f;
-    const float d2 = 1.f;
-     
-    for(size_t i = 0; i < numberOfSamplesPerClass; ++i)
-    {
-      for(int j = 0; j < labelSetVector.size(); ++j)
-      {
-        float angle = j*rotation_per_class;
-        float r11 = cos(angle);
-        float r12 = -sin(angle);
-        float r21 = sin(angle);
-        float r22 = cos(angle);
-          
-        //Rotation matrix (j*(2*pi/numberOfSamplesPerClass)); R = [cos(x) -sin(x); sin(x) cos(x)];
-        float a = r11*d1 + r12*d2;
-        float b = r21*d1 + r22*d2;
-        
-        Example_CPtr e(new Example<Label>(make_const_2d_descriptor(a,b), labelSetVector[j]));
-        exampleSet.push_back( e );
-      }
-    }
-    
-    return exampleSet;
-  }
-};
-
-
   template <typename Label>
   class UnitCircleExampleGenerator
   {
