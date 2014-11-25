afd281c50c7e7d599f9e1e9c507558e940954904
mikesapi <mikesapi.code.email@gmail.com>
2014-11-25 18:21:57 +0000

    Cleaning up UnitCircleExampleGenerator.h

--- modules/rafl/include/rafl/examples/UnitCircleExampleGenerator.h
+++ modules/rafl/include/rafl/examples/UnitCircleExampleGenerator.h
@@ -5,6 +5,9 @@
 #ifndef H_RAFL_UNITCIRCLEEXAMPLEGENERATOR
 #define H_RAFL_UNITCIRCLEEXAMPLEGENERATOR
 
+#include <cmath>
+#include <set>
+
 #include <Eigen/Dense>
 using Eigen::ArrayXf;
 using Eigen::Vector2f;
@@ -12,8 +15,6 @@ using Eigen::VectorXf;
 using Eigen::Matrix2f;
 
 #include "ExampleUtil.h"
-
-#include <set>
 
 namespace rafl {
 
@@ -29,22 +30,24 @@ private:
 
   //#################### PRIVATE VARIABLES ####################
 private:
-  float m_d1;
-  float m_d2;
+  Vector2f m_referencePoint;
 
-  //Defines the angle with which to rotate the unit vector [d1 d2]' 
+  Matrix2f m_2dRotationMatrix;
+
+  //Defines the angle with which to rotate the reference unit vector. 
   float m_rotationPerClass;
  
   std::map<Label,int> m_uniqueClassLabels;
 
-  float m_pi;
-
   //#################### CONSTRUCTORS ####################
 public:
   explicit UnitCircleExampleGenerator(std::map<Label,int> uniqueClassLabels)
-  : m_d1(0.0f), m_d2(1.0f), m_pi(3.14f), m_uniqueClassLabels(uniqueClassLabels)
+  : m_uniqueClassLabels(uniqueClassLabels)
   {
-    m_rotationPerClass = 2.0f*m_pi/m_uniqueClassLabels.size();
+    m_referencePoint(0) = 0.0f;
+    m_referencePoint(1) = 1.0f;
+
+    m_rotationPerClass = 2.0f*M_PI/m_uniqueClassLabels.size();
   }
 
   //#################### PUBLIC MEMBER FUNCTIONS ####################
@@ -59,23 +62,18 @@ public:
   std::vector<Example_CPtr> generate_examples_in_set(const std::set<Label>& labelSet, size_t numberOfSamplesPerClass)
   {
     std::vector<Example_CPtr> exampleSet;
-    //std::vector<Label> labelSetVector(labelSet.begin(), labelSet.end());
-
-    //typename std::set<Label>::const_iterator set_iterator = labelSet.begin();
      
     for(size_t i = 0; i < numberOfSamplesPerClass; ++i)
     {
       for(typename std::set<Label>::const_iterator it = labelSet.begin(), iend = labelSet.end(); it != iend; ++it)
       {
         float angle = m_uniqueClassLabels[*it]*m_rotationPerClass;
-        float r11 = cos(angle);
-        float r12 = -sin(angle);
-        float r21 = sin(angle);
-        float r22 = cos(angle);
-          
-        //Rotation matrix (j*(2*pi/numberOfSamplesPerClass)); R = [cos(x) -sin(x); sin(x) cos(x)];
-        float a = r11*m_d1 + r12*m_d2;
-        float b = r21*m_d1 + r22*m_d2;
+        m_2dRotationMatrix = Eigen::Rotation2Df(angle);
+        
+        Vector2f rotatedPoint = (m_referencePoint.transpose()*m_2dRotationMatrix).transpose();
+  
+        float a = rotatedPoint(0); // + Gaussian Noise
+        float b = rotatedPoint(1); // + Gaussian Noise
         std::cout << "(" << a << "," << b << ")\n";
         
         Example_CPtr e(new Example<Label>(ExampleUtil::make_const_2d_descriptor(a,b), *it));
