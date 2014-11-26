5a05f943791896954abeb4b8932f687dcf959a5c
Stuart Golodetz <sgolodetz@gxstudios.net>
2014-11-26 14:08:31 +0000

    Continue working on the Vicon test.

--- tests/scratch/vicon/main.cpp
+++ tests/scratch/vicon/main.cpp
@@ -4,23 +4,58 @@
 
 #include <cstddef>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 #include <vicon/Client.h>
 using namespace ViconDataStreamSDK::CPP;
 
-int main()
+class Tracker
 {
-  std::string hostname = "localhost:801";
+  //#################### PRIVATE VARIABLES ####################
+private:
+  Client m_vicon;
 
-  Client client;
-  while(!client.IsConnected().Connected)
+  //#################### CONSTRUCTORS ####################
+public:
+  explicit Tracker(const std::string& hostname)
   {
-    if(client.Connect(hostname).Result != Result::Success)
+    if(m_vicon.Connect(hostname).Result != Result::Success)
     {
-      std::cout << "Error: Could not connect to the Vicon system!" << std::endl;
-      return EXIT_FAILURE;
+      throw std::runtime_error("Error: Could not connect to the Vicon system!");
     }
+
+    std::cout << "Connected to the Vicon system!" << std::endl;
+
+    m_vicon.EnableMarkerData();
+    m_vicon.EnableSegmentData();
+    m_vicon.EnableUnlabeledMarkerData();
+
+    m_vicon.SetStreamMode(ViconDataStreamSDK::CPP::StreamMode::ServerPush);
+
+    m_vicon.SetAxisMapping(Direction::Forward, Direction::Left, Direction::Up);
   }
+
+  //#################### DESTRUCTOR ####################
+public:
+  ~Tracker()
+  {
+    m_vicon.DisableUnlabeledMarkerData();
+    m_vicon.DisableSegmentData();
+    m_vicon.DisableMarkerData();
+    m_vicon.Disconnect();
+  }
+};
+
+int main()
+try
+{
+  std::string hostname = "192.168.0.107:801";
+  Tracker tracker(hostname);
   return 0;
+}
+catch(std::exception& e)
+{
+  std::cerr << e.what() << '\n';
+  return EXIT_FAILURE;
 }
