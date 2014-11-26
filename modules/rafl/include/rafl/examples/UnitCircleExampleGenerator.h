0993179ba6e2b167dd63c81efdf96dbbd8b11e92
mikesapi <mikesapi.code.email@gmail.com>
2014-11-26 12:02:06 +0000

    Commented UnitCircleExampleGenerator

--- modules/rafl/include/rafl/examples/UnitCircleExampleGenerator.h
+++ modules/rafl/include/rafl/examples/UnitCircleExampleGenerator.h
@@ -16,9 +16,9 @@ using Eigen::Matrix2f;
 
 namespace rafl {
 
-  /**
-   * \brief This class generates examples drawn from points equally spaced around the unit circle.
-   */
+/**
+ * \brief This class generates examples drawn from points equally spaced around the unit circle.
+ */
 template <typename Label>
 class UnitCircleExampleGenerator
 {
@@ -28,16 +28,26 @@ private:
 
   //#################### PRIVATE VARIABLES ####################
 private:
+  /** The `seed' unit vector which is rotated to create seed points for multiple classes around the unit circle. */
   Vector2f m_referencePoint;
 
+  /** A matrix used to rotate the seed reference vector. */
   Matrix2f m_2dRotationMatrix;
 
-  //Defines the angle with which to rotate the reference unit vector. 
+  /** Defines the angular separation between seed points of distinct classes. */ 
   float m_rotationPerClass;
  
+  /** A mapping from the entire set of unique labels to integers which may be compared and handeled more easily than
+   * general class labels */
   std::map<Label,int> m_uniqueClassLabels;
 
   //#################### CONSTRUCTORS ####################
+  /**
+  * \brief Constructs a UnitCircleExampleGenerator 
+  *
+  * \param uniqueClassLabels  Defines the entire unique set of labels and an ordering between the labels implicit in the
+  * integer value it maps to.
+  */
 public:
   explicit UnitCircleExampleGenerator(std::map<Label,int> uniqueClassLabels)
   : m_uniqueClassLabels(uniqueClassLabels)
@@ -50,13 +60,13 @@ public:
 
   //#################### PUBLIC MEMBER FUNCTIONS ####################
 public:
-/*
- * \brief Creates a vector of examples with the same number of examples per class
- *
- * \param labelSet                  The set of labels from which to draw the samples
- * \param numberOfSamplesPerClass   The number of samples to create per class
- * \return                          The set of generated examples
- */
+  /*
+   * \brief Generates examples from the entire label set or a subset of labels
+   *
+   * \param labelSet                  The set of labels from which to draw the samples
+   * \param numberOfSamplesPerClass   The number of samples to create per class
+   * \return                          The set of generated examples
+   */
   std::vector<Example_CPtr> generate_examples_in_set(const std::set<Label>& labelSet, size_t numberOfSamplesPerClass)
   {
     std::vector<Example_CPtr> exampleSet;
@@ -72,8 +82,11 @@ public:
   
         float a = rotatedPoint(0); // + Gaussian Noise
         float b = rotatedPoint(1); // + Gaussian Noise
+
+        #if 0
         std::cout << "(" << a << "," << b << ")\n";
-        
+        #endif
+
         Example_CPtr e(new Example<Label>(ExampleUtil::make_const_2d_descriptor(a,b), *it));
         exampleSet.push_back( e );
       }
