ea1f2e08187acf6f2c212e52324b87e5b274b932
mikesapi <mikesapi.code.email@gmail.com>
2014-12-04 17:00:35 +0000

    modified the scratch test to include some unaries

--- tests/scratch/infermous/main.cpp
+++ tests/scratch/infermous/main.cpp
@@ -13,7 +13,19 @@ int main()
     std::cout << it.x() << ' ' << it.y() << '\n';
     CRF2D<int>::Node& n = *it;
   }*/
+  typedef int Label;
   CRF2D<int>::ProbabilitiesGrid_Ptr unariesGrid(new CRF2D<int>::ProbabilitiesGrid(5, 5));
+
+  std::map<Label,float> unaries;
+  for(size_t i = 0; i < 5; ++i)
+    unaries.insert(std::make_pair(i,0.5));
+  
+  for(size_t i = 0; i < 5; ++i)
+    for(size_t j = 0; j < 5; ++j)
+    {
+      (*unariesGrid)(i,j) = unaries;
+    }
+
   CRF2D<int> crf(unariesGrid, 3);
 
   crf.update();
