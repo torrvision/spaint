37d92c2f88dc21852b66734bbddf920ea36be989
Stuart Golodetz <sgolodetz@gxstudios.net>
2014-12-09 10:45:34 +0000

    Comment ArgUtil.

--- modules/tvgutil/include/tvgutil/ArgUtil.h
+++ modules/tvgutil/include/tvgutil/ArgUtil.h
@@ -12,16 +12,19 @@
 namespace tvgutil {
 
 /**
- * \brief TODO
+ * \brief This class contains utility functions for performing argmax and argmin operations.
  */
 class ArgUtil
 {
   //#################### PREDICATES ####################
 private:
-  template <typename K, typename V, typename BasePred>
+  /**
+   * \brief An instance of an instantiation of this predicate can be used to perform comparisons on the second elements of pairs.
+   */
+  template <typename T1, typename T2, typename BasePred>
   struct SndPred
   {
-    bool operator()(const std::pair<K,V>& lhs, const std::pair<K,V>& rhs) const
+    bool operator()(const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs) const
     {
       BasePred basePred;
       return basePred(lhs.second, rhs.second);
@@ -30,12 +33,30 @@ private:
 
   //#################### PUBLIC STATIC MEMBER FUNCTIONS ####################
 public:
+  /**
+   * \brief Calculates argmax_k m[k].
+   *
+   * This function finds a key in the map whose corresponding value is largest (using std::greater).
+   * If there are several keys with the largest value, one of them is returned deterministically.
+   *
+   * \param m The map over which to perform the argmax.
+   * \return  A key in the map with the largest corresponding value.
+   */
   template <typename K, typename V>
   static const K& argmax(const std::map<K,V>& m)
   {
     return std::min_element(m.begin(), m.end(), SndPred<K,V,std::greater<V> >())->first;
   }
 
+  /**
+   * \brief Calculates argmin_k m[k].
+   *
+   * This function finds a key in the map whose corresponding value is smallest (using std::less).
+   * If there are several keys with the smallest value, one of them is returned deterministically.
+   *
+   * \param m The map over which to perform the argmin.
+   * \return  A key in the map with the smallest corresponding value.
+   */
   template <typename K, typename V>
   static const K& argmin(const std::map<K,V>& m)
   {
