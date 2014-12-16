e957cb294d82a616c014589c840f74c6f9c2f5a9
mikesapi <mikesapi.code.email@gmail.com>
2014-12-16 11:57:29 +0000

    Suggestion: Added a Timing class and TIMERX macro to allow profiling of various methods within the same scope, see scratch test, if it passes first round of approvals I will comment it ;)

--- modules/tvgutil/include/tvgutil/Timing.h
+++ modules/tvgutil/include/tvgutil/Timing.h
@@ -30,3 +30,42 @@
   totalDur += thisDur; \
   ++durCount; \
   boost::chrono::scale dur = totalDur / durCount;
+
+#define TIMEX(target, scale, tag) \
+  Timing<boost::chrono::scale> tag(#tag); \
+  target; \
+  tag.stop();
+
+template <typename T>
+class Timing
+{
+private:
+  T m_duration;
+  std::string m_name;
+  boost::chrono::high_resolution_clock::time_point m_t0, m_t1;
+
+public:
+  Timing(std::string name)
+  : m_name(name)
+  {
+    m_t0 = boost::chrono::high_resolution_clock::now();
+  }
+
+  T duration() const
+  {
+    return m_duration;
+  }
+
+  void print()
+  {
+    std::cout << m_name << ": " << m_duration << "\n";
+  }
+
+  void stop()
+  {
+    m_t1 = boost::chrono::high_resolution_clock::now();
+    m_duration = boost::chrono::duration_cast<T>(m_t1 - m_t0);
+  }
+};
+
+
--- tests/scratch/tvgutil/main.cpp
+++ tests/scratch/tvgutil/main.cpp
@@ -1,5 +1,44 @@
+#if 0
 int main()
 {
   // TODO
   return 0;
 }
+#endif
+
+#if 1
+
+#include <iostream>
+#include <string>
+
+#include <tvgutil/Timing.h>
+
+class Dummy
+{
+public:
+  Dummy(size_t time)
+  {
+    std::cout << "I'm very tired.. going for a nap of " << std::to_string(time) << " seconds..\n";
+    sleep(time);
+  }
+  void snooze(size_t time)
+  {
+    std::cout << "I'm very tired.. going for a snooze of " << std::to_string(time) << " seconds..\n";
+    sleep(time);
+  }
+};
+
+
+int main()
+{
+  std::cout << "Testing the timing features!\n";
+
+  TIMEX(Dummy d1(2), milliseconds, dummyConstructor)
+  dummyConstructor.print();
+
+  TIMEX(d1.snooze(3), milliseconds, dummySnooze)
+  dummySnooze.print();
+
+  return 0;
+}
+#endif
