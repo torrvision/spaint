6affac4a610bb944d30199c86b52e6514fceb6ff
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-01-07 12:28:30 +0000

    Add a missing comment to PairwisePotentialCalculator.

--- modules/infermous/include/infermous/base/PairwisePotentialCalculator.h
+++ modules/infermous/include/infermous/base/PairwisePotentialCalculator.h
@@ -25,7 +25,13 @@ public:
   //#################### PUBLIC ABSTRACT MEMBER FUNCTIONS ####################
 public:
   /**
-   * \brief TODO
+   * \brief Calculates the pairwise potential between two nodes in the CRF based on their labels.
+   *
+   * Note that this should be symmetric, i.e. calculate_potential(l1,l2) should equal calculate_potential(l2,l1).
+   *
+   * \param l1  The label of one of the nodes.
+   * \param l2  The label of the other node.
+   * \return    The calculated pairwise potential between the two nodes.
    */
   virtual float calculate_potential(const Label& l1, const Label& l2) const = 0;
 };
