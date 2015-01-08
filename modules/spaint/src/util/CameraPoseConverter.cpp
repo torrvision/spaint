5522edbca8f26e9da40f6301601fa025d0ed317e
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-01-08 23:44:48 +0000

    Sort out the commenting in CameraPoseConverter.

--- modules/spaint/src/util/CameraPoseConverter.cpp
+++ modules/spaint/src/util/CameraPoseConverter.cpp
@@ -20,38 +20,43 @@ ITMPose CameraPoseConverter::camera_to_pose(const Camera& camera)
   const Eigen::Vector3f& v = camera.v();
 
   /*
-  Calculate the model-view matrix corresponding to the camera. This should be a matrix that maps
-  points from  world XYZ coordinates to camera XYZ coordinates. In practice, our cameras use UVN
-  coordinates rather than XYZ coordinates, such that u = -x and v = -y. We will see how to account
-  for this shortly.
+  Calculate the InfiniTAM pose matrix corresponding to the camera. This is the matrix that transforms
+  points in the space corresponding to the initial pose of the camera in InfiniTAM (n = (0,0,1)^T,
+  u = (-1,0,0)^T, v = (0,-1,0)^T, p = (0,0,0)^T) into the space corresponding to the current pose
+  of the camera. (Alternatively, it can be seen as the matrix that maps the current camera axes to
+  the initial camera axes when both are expressed in world coordinates.)
 
-  The first step is to calculate a matrix mapping points from world XYZ coordinates to camera UVN
-  coordinates. This can alternatively be seen as a matrix mapping the camera's UVN axes into the
-  world's XYZ axes. As such, it is the inverse of the following matrix, which maps the world's XYZ
-  axes into the camera's UVN axes:
+  Notationally, let I be the initial camera space, C be the current camera space and W be the world
+  space. Furthermore, let bTa denote the transformation that maps points from space a to space b.
+  Thus cTi denotes the pose matrix, namely the matrix that transforms points from initial camera
+  space to current camera space, and cTw = cTi . iTw denotes the model-view matrix that would be
+  used in OpenGL. The matrix cTw is easy to calculate, and can be used to calculate the required
+  pose matrix via cTi = cTw . iTw^-1 = cTw . wTi.
+
+  To calculate cTw, the matrix that maps world space points to camera space ones, first calculate
+  its inverse wTc, the matrix that maps camera space points to world space ones. This is trivial:
 
   (ux vx nx px)
   (uy vy ny py)
   (uz vz nz pz)
   ( 0  0  0  1)
 
-  This inverse is:
+  From this we can obtain cTw:
 
   (ux uy uz -p.u)
   (vx vy vz -p.v)
   (nx ny nz -p.n)
   ( 0  0  0    1)
 
-  This matrix maps points from world XYZ coordinates to camera UVN coordinates, but what we actually
-  want is to map to camera XYZ coordinates. To do this, we must premultiply the matrix we have just
-  derived by a matrix that transforms from camera UVN coordinates to camera XYZ coordinates, namely:
+  What is wTi, the matrix mapping points in initial camera space to points in world space? Well,
+  bearing in mind the initial pose of the camera, it must be:
 
-  (1  0  0 0)
-  (0 -1  0 0)
-  (0  0 -1 0)
-  (0  0  0 1)
+  (-1  0 0 0)
+  ( 0 -1 0 0)
+  ( 0  0 1 0)
+  ( 0  0 0 1)
 
-  The resulting matrix is then:
+  We can thus easily obtain the required pose matrix cTi by matrix multiplication:
 
   (-ux -uy -uz  p.u)
   (-vx -vy -vz  p.v)
@@ -62,7 +67,10 @@ ITMPose CameraPoseConverter::camera_to_pose(const Camera& camera)
   pose.R(0,1) = -v.x(); pose.R(1,1) = -v.y(); pose.R(2,1) = -v.z(); pose.T.y = p.dot(v);
   pose.R(0,2) = n.x();  pose.R(1,2) = n.y();  pose.R(2,2) = n.z();  pose.T.z = -p.dot(n);
 
-  // Correctly set the rest of the InfiniTAM pose from the model-view matrix.
+  // Correctly set the rest of the InfiniTAM pose structure from the calculated pose matrix.
+  // Note that the use of "ModelView" in the names of the ITMPose member functions refers
+  // to the InfiniTAM pose matrix, and NOT the OpenGL model-view matrix. This is a crucial
+  // distinction that can cause much confusion!
   pose.SetParamsFromModelView();
   pose.SetModelViewFromParams();
 
