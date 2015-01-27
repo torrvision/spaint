48a76ac37ed1e9fbce762a67491d466ac7997aea
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-01-27 15:13:18 +0000

    Make a minor optimisation.

--- modules/spaint/src/visualisers/multiplatform/cpu/SemanticVisualiser_CPU.cpp
+++ modules/spaint/src/visualisers/multiplatform/cpu/SemanticVisualiser_CPU.cpp
@@ -14,6 +14,7 @@ void SemanticVisualiser_CPU::render(const ITMLib::Objects::ITMScene<SpaintVoxel,
                                     const ITMLib::Objects::ITMIntrinsics *intrinsics, const ITMLib::Objects::ITMRenderState *renderState,
                                     ITMUChar4Image *outputImage) const
 {
+  // Set up the label colours (quick hack).
   Vector3u labelColours[] =
   {
     Vector3u(255, 255, 255),
@@ -22,7 +23,7 @@ void SemanticVisualiser_CPU::render(const ITMLib::Objects::ITMScene<SpaintVoxel,
     Vector3u(0, 0, 255)
   };
 
-  Vector2i imgSize = outputImage->noDims;
+  int imgSize = outputImage->noDims.x * outputImage->noDims.y;
   Vector3f lightSource = -Vector3f(pose->GetInvM().getColumn(2));
   Vector4u *outRendering = outputImage->GetData(MEMORYDEVICE_CPU);
   const Vector4f *pointsRay = renderState->raycastResult->GetData(MEMORYDEVICE_CPU);
@@ -32,7 +33,7 @@ void SemanticVisualiser_CPU::render(const ITMLib::Objects::ITMScene<SpaintVoxel,
 #ifdef WITH_OPENMP
   #pragma omp parallel for
 #endif
-  for (int locId = 0; locId < imgSize.x * imgSize.y; ++locId)
+  for (int locId = 0; locId < imgSize; ++locId)
   {
     Vector4f ptRay = pointsRay[locId];
     processPixelSemantic(outRendering[locId], ptRay.toVector3(), ptRay.w > 0, voxelData, voxelIndex, lightSource, labelColours);
