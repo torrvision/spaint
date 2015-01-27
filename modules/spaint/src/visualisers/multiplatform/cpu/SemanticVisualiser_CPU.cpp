7971c923b82593798ddf5c2e9102f29e1504cce7
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-01-27 15:10:36 +0000

    Tidy up the CPU semantic visualiser and make it use OpenMP.

--- modules/spaint/src/visualisers/multiplatform/cpu/SemanticVisualiser_CPU.cpp
+++ modules/spaint/src/visualisers/multiplatform/cpu/SemanticVisualiser_CPU.cpp
@@ -14,34 +14,6 @@ void SemanticVisualiser_CPU::render(const ITMLib::Objects::ITMScene<SpaintVoxel,
                                     const ITMLib::Objects::ITMIntrinsics *intrinsics, const ITMLib::Objects::ITMRenderState *renderState,
                                     ITMUChar4Image *outputImage) const
 {
-  // TODO: Get rid of this bit (after checking to make sure that's ok).
-  const SpaintVoxel *voxelData = scene->localVBA.GetVoxelBlocks();
-  const ITMVoxelIndex::IndexData *voxelIndex = scene->index.getIndexData();
-
-  Vector2i imgSize = outputImage->noDims;
-  float oneOverVoxelSize = 1.0f / scene->sceneParams->voxelSize;
-
-  Matrix4f invM = pose->GetInvM();
-  Vector4f projParams = intrinsics->projectionParamsSimple.all;
-  projParams.x = 1.0f / projParams.x;
-  projParams.y = 1.0f / projParams.y;
-
-  float mu = scene->sceneParams->mu;
-  Vector3f lightSource = -Vector3f(invM.getColumn(2));
-
-  Vector4u *outRendering = outputImage->GetData(MEMORYDEVICE_CPU);
-  const Vector2f *minmaximg = renderState->renderingRangeImage->GetData(MEMORYDEVICE_CPU);
-  Vector4f *pointsRay = renderState->raycastResult->GetData(MEMORYDEVICE_CPU);
-
-  for (int y = 0; y < imgSize.y; y++) for (int x = 0; x < imgSize.x; x++)
-  {
-    int locId = x + y * imgSize.x;
-    int locId2 = (int)floor((float)x / minmaximg_subsample) + (int)floor((float)y / minmaximg_subsample) * imgSize.x;
-
-    castRay<SpaintVoxel,ITMVoxelIndex>(pointsRay[locId], x, y, voxelData, voxelIndex, invM, projParams, oneOverVoxelSize, mu, minmaximg[locId2]);
-  }
-  // END TODO
-
   Vector3u labelColours[] =
   {
     Vector3u(255, 255, 255),
@@ -50,6 +22,16 @@ void SemanticVisualiser_CPU::render(const ITMLib::Objects::ITMScene<SpaintVoxel,
     Vector3u(0, 0, 255)
   };
 
+  Vector2i imgSize = outputImage->noDims;
+  Vector3f lightSource = -Vector3f(pose->GetInvM().getColumn(2));
+  Vector4u *outRendering = outputImage->GetData(MEMORYDEVICE_CPU);
+  const Vector4f *pointsRay = renderState->raycastResult->GetData(MEMORYDEVICE_CPU);
+  const SpaintVoxel *voxelData = scene->localVBA.GetVoxelBlocks();
+  const ITMVoxelIndex::IndexData *voxelIndex = scene->index.getIndexData();
+
+#ifdef WITH_OPENMP
+  #pragma omp parallel for
+#endif
   for (int locId = 0; locId < imgSize.x * imgSize.y; ++locId)
   {
     Vector4f ptRay = pointsRay[locId];
