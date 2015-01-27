8e253505f406bc302cea653fa6ee641032e8d57c
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-01-27 15:37:03 +0000

    Tidy up SpaintVoxel (it will need further work though).

--- modules/spaint/include/spaint/util/SpaintVoxel.h
+++ modules/spaint/include/spaint/util/SpaintVoxel.h
@@ -9,7 +9,6 @@
 
 namespace spaint {
 
-#if 1
 /**
  * \brief An instance of this struct represents a voxel in the reconstructed scene.
  */
@@ -32,42 +31,9 @@ struct SpaintVoxel
   {
     sdf = SDF_initialValue();
     w_depth = 0;
-    label = 2;
+    label = 0;
   }
 };
-#else
-struct SpaintVoxel
-{
-  // TEMPORARY
-
-	_CPU_AND_GPU_CODE_ static short SDF_initialValue() { return 32767; }
-	_CPU_AND_GPU_CODE_ static float SDF_valueToFloat(float x) { return (float)(x) / 32767.0f; }
-	_CPU_AND_GPU_CODE_ static short SDF_floatToValue(float x) { return (short)((x) * 32767.0f); }
-
-	static const CONSTANT(bool) hasColorInformation = true;
-
-	/** Value of the truncated signed distance transformation. */
-	short sdf;
-	/** Number of fused observations that make up @p sdf. */
-	uchar w_depth;
-	/** Padding that may or may not improve performance on certain GPUs */
-	//uchar pad;
-	/** RGB colour information stored for this voxel. */
-	Vector3u clr;
-	/** Number of observations that made up @p clr. */
-	uchar w_color;
-
-	_CPU_AND_GPU_CODE_ SpaintVoxel()
-	{
-		sdf = SDF_initialValue();
-		w_depth = 0;
-		clr = (uchar)0;
-		w_color = 0;
-	}
-
-  // END TEMPORARY
-};
-#endif
 
 }
 
