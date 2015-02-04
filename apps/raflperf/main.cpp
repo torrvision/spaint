3f03010be17db88954cb4224e69b49c073429fb7
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-02-04 18:24:52 +0000

    Tidy up raflperf.

--- apps/raflperf/main.cpp
+++ apps/raflperf/main.cpp
@@ -2,8 +2,6 @@
  * raflperf: main.cpp
  */
 
-#include <cmath>
-
 #include <boost/assign/list_of.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 using boost::assign::list_of;
@@ -21,11 +19,19 @@ using namespace rafl;
 
 #include "RandomForestEvaluator.h"
 
+//#################### TYPEDEFS ####################
+
 typedef int Label;
 typedef boost::shared_ptr<const Example<Label> > Example_CPtr;
-
 typedef CartesianProductParameterSetGenerator::ParamSet ParamSet;
 
+//#################### FUNCTIONS ####################
+
+/**
+ * \brief Gets the current time in ISO format.
+ *
+ * \return  The current time in ISO format.
+ */
 std::string get_iso_timestamp()
 {
   boost::posix_time::ptime currentDateTime(boost::posix_time::second_clock::local_time());
@@ -34,6 +40,8 @@ std::string get_iso_timestamp()
 
 int main(int argc, char *argv[])
 {
+  const unsigned int seed = 1234;
+
   if(argc != 1 && argc != 4)
   {
     std::cerr << "Usage: raflperf [<training set file> <test set file> <output path>]\n";
@@ -45,15 +53,15 @@ int main(int argc, char *argv[])
 
   if(argc == 1)
   {
-    //Generate a set of labels.
+    // Generate a set of labels.
     std::set<Label> classLabels;
     classLabels.insert(1);
     classLabels.insert(3);
     classLabels.insert(5);
     classLabels.insert(7);
 
-    //Generate examples around the unit circle.
-    UnitCircleExampleGenerator<Label> uceg(classLabels, 1234);
+    // Generate examples around the unit circle.
+    UnitCircleExampleGenerator<Label> uceg(classLabels, seed);
     examples = uceg.generate_examples(classLabels, 100);
     outputResultPath = "UnitCircleExampleGenerator-Results.txt";
   }
@@ -63,55 +71,61 @@ int main(int argc, char *argv[])
     std::string testingSetPath = argv[2];
     outputResultPath = argv[3];
 
-    std::cout << "Training set: " << trainingSetPath << "\n";
-    std::cout << "Testing set: " << testingSetPath << "\n";
+    std::cout << "Training set: " << trainingSetPath << '\n';
+    std::cout << "Testing set: " << testingSetPath << '\n';
 
     examples = ExampleUtil::load_examples<Label>(trainingSetPath);
     std::vector<Example_CPtr> testingExamples = ExampleUtil::load_examples<Label>(testingSetPath);
 
-    examples.insert(examples.end(), testingExamples.begin(), testingExamples.end() );
-    std::cout << "number of examples= " << examples.size() << "\n";
+    examples.insert(examples.end(), testingExamples.begin(), testingExamples.end());
+    std::cout << "Number of examples = " << examples.size() << '\n';
   }
 
-  //Generate parameters of your algorithm.
+  // Generate the parameter sets with which to test the random forest.
   std::vector<ParamSet> params = CartesianProductParameterSetGenerator()
     .add_param("treeCount", list_of<size_t>(1)(5)(9)(13))
-    .add_param("splitBudget", list_of<size_t>(static_cast<size_t>(pow(2,10)))(static_cast<size_t>(pow(2,20))))
+    .add_param("splitBudget", list_of<size_t>(1024)(1048576))
     .add_param("candidateCount", list_of<int>(256))
     .add_param("decisionFunctionGeneratorType", list_of<std::string>("FeatureThresholding"))
     .add_param("gainThreshold", list_of<float>(0.0f))
     .add_param("maxClassSize", list_of<size_t>(10000))
     .add_param("maxTreeHeight", list_of<size_t>(20))
-    .add_param("randomSeed", list_of<unsigned int>(1234))
+    .add_param("randomSeed", list_of<unsigned int>(seed))
     .add_param("seenExamplesThreshold", list_of<size_t>(50))
     .add_param("splittabilityThreshold", list_of<float>(0.5f))
     .generate_param_sets();
 
-  const size_t numFolds = 2;
-  const unsigned int seed = 1234;
+  // Construct the split generator.
+#if 0
+  const size_t foldCount = 2;
+  SplitGenerator_Ptr splitGenerator(new CrossValidationSplitGenerator(seed, foldCount));
+#else
+  const size_t splitCount = 5;
+  const float ratio = 0.5f;
+  SplitGenerator_Ptr splitGenerator(new RandomPermutationAndDivisionSplitGenerator(seed, splitCount, ratio));
+#endif
 
+  // Evaluate the random forest on the various different parameter sets.
   PerformanceTable results(list_of("Accuracy"));
-  //SplitGenerator_Ptr splitGenerator(new CrossValidationSplitGenerator(seed, numFolds));
-  SplitGenerator_Ptr splitGenerator(new RandomPermutationAndDivisionSplitGenerator(seed, 5, 0.5f));
   boost::shared_ptr<RandomForestEvaluator<Label> > evaluator;
-  for(size_t n = 0, nend = params.size(); n < nend; ++n)
+  for(size_t n = 0, size = params.size(); n < size; ++n)
   {
     evaluator.reset(new RandomForestEvaluator<Label>(splitGenerator, params[n]));
     std::map<std::string,PerformanceMeasure> result = evaluator->evaluate(examples);
     results.record_performance(params[n], result);
   }
 
-  //Output results to the screen.
+  // Output the performance table to the screen.
   results.output(std::cout);
 
-  //Time-stamp the results file.
+  // Time-stamp the results file.
   outputResultPath += "-" + get_iso_timestamp();
 
-  //Output results to a file.
+  // Output the performance table to the results file.
   std::ofstream resultsFile(outputResultPath.c_str());
   if(!resultsFile)
   {
-    std::cout << "Warning could not open file for writing..\n";
+    std::cout << "Warning could not open file for writing...\n";
   }
   else
   {
@@ -120,4 +134,3 @@ int main(int argc, char *argv[])
 
   return 0;
 }
-
