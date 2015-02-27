bb98c1c766cb3e05861ed4ab5927a0989b0d1a55
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-02-27 14:27:33 +0000

    Make it possible to switch between different selectors.

--- modules/spaint/src/core/SpaintInteractor.cpp
+++ modules/spaint/src/core/SpaintInteractor.cpp
@@ -56,7 +56,7 @@ void SpaintInteractor::mark_voxels(const Selection_CPtr& selection, unsigned cha
 
 SpaintInteractor::Selection_CPtr SpaintInteractor::select_voxels(const InputState& inputState, const RenderState_CPtr& renderState) const
 {
-  return m_selector->select_voxels(inputState, renderState);
+  return m_selector ? m_selector->select_voxels(inputState, renderState) : Selection_CPtr();
 }
 
 void SpaintInteractor::set_semantic_label(unsigned char semanticLabel)
@@ -66,7 +66,15 @@ void SpaintInteractor::set_semantic_label(unsigned char semanticLabel)
 
 void SpaintInteractor::update_selector(const InputState& inputState)
 {
-  m_selector->update(inputState);
+  // Allow the user to switch between different selectors.
+  if(inputState.key_down(SDLK_i))
+  {
+    if(inputState.key_down(SDLK_1)) m_selector.reset();
+    else if(inputState.key_down(SDLK_2)) m_selector.reset(new PickingSelector(m_model->get_settings()));
+  }
+
+  // Allow the user to update the parameters of the current selector.
+  if(m_selector) m_selector->update(inputState);
 }
 
 }
