6bea1bb1ce41f35e16c678aae81ce7d218df53e8
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-04-16 23:10:43 +0100

    Get rid of some unnecessary whitespace.

--- modules/spaint/include/spaint/visualisers/interface/SemanticVisualiser.h
+++ modules/spaint/include/spaint/visualisers/interface/SemanticVisualiser.h
@@ -47,12 +47,12 @@ public:
   /**
    * \brief Renders a semantic view of the specified scene from the specified camera pose.
    *
-   * \param scene         The scene.
-   * \param pose          The camera pose.
-   * \param intrinsics    The intrinsic parameters of the camera.
-   * \param renderState   The render state corresponding to the specified camera pose.
-   * \param usePhong      Whether or not to use Phong lighting.
-   * \param outputImage   The image into which to write the semantic visualisation of the scene.
+   * \param scene       The scene.
+   * \param pose        The camera pose.
+   * \param intrinsics  The intrinsic parameters of the camera.
+   * \param renderState The render state corresponding to the specified camera pose.
+   * \param usePhong    Whether or not to use Phong lighting.
+   * \param outputImage The image into which to write the semantic visualisation of the scene.
    */
   virtual void render(const ITMLib::Objects::ITMScene<SpaintVoxel,ITMVoxelIndex> *scene, const ITMLib::Objects::ITMPose *pose,
                       const ITMLib::Objects::ITMIntrinsics *intrinsics, const ITMLib::Objects::ITMRenderState *renderState,
