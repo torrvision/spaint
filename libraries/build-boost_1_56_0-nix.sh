d2c04b79d198d5503c7a7737f335858e4ce76968
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-04-24 14:06:44 +0100

    Make the Unix version of the Boost build script apply a patch to make it possible to serialize shared pointers to const objects.

--- libraries/build-boost_1_56_0-nix.sh
+++ libraries/build-boost_1_56_0-nix.sh
@@ -54,9 +54,11 @@ fi
 
 ./b2 -j2 --libdir=../boost_1_56_0/lib --includedir=../boost_1_56_0/include --abbreviate-paths --with-chrono --with-date_time --with-filesystem --with-regex --with-serialization --with-test --with-thread --build-type=complete --layout=tagged toolset=$TOOLSET architecture=x86 address-model=64 $STDLIBFLAGS install >> $LOG
 
+echo "[spaint] ...Fixing headers..."
+perl -i -pe 's/SPT<void>/SPT<const void>/g' ../boost_1_56_0/include/boost/serialization/shared_ptr_helper.hpp
+
 if [ $PLATFORM == "mac" ]
 then
-  echo "[spaint] ...Fixing headers..."
   perl -i -pe 's/INT128__\)$/INT128__) && !defined(__CUDACC__)/g' ../boost_1_56_0/include/boost/config/compiler/clang.hpp
   perl -i -pe 's/~this_type\(\);$/~sp_counted_impl_pda<P, D, A>();/g' ../boost_1_56_0/include/boost/smart_ptr/detail/sp_counted_impl.hpp
 fi
