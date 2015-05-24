53fc3e65afb0580e1e136119e07c37ce2c80d4de
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-05-24 18:53:50 +0100

    Implement FrameBuffer.

---
+++ modules/spaint/include/spaint/ogl/FrameBuffer.h
@@ -0,0 +1,68 @@
+/**
+ * spaint: FrameBuffer.h
+ */
+
+#ifndef H_SPAINT_FRAMEBUFFER
+#define H_SPAINT_FRAMEBUFFER
+
+#include "WrappedGL.h"
+
+namespace spaint {
+
+/**
+ * \brief An instance of this class represents an off-screen frame buffer to which OpenGL rendering calls can be directed.
+ */
+class FrameBuffer
+{
+  //#################### PRIVATE VARIABLES ####################
+private:
+  /** The texture ID of the frame buffer's colour buffer. */
+  GLuint m_colourBufferID;
+
+  /** The render buffer ID of the frame buffer's depth buffer. */
+  GLuint m_depthBufferID;
+
+  /** The ID of the frame buffer itself. */
+  GLuint m_id;
+
+  //#################### CONSTRUCTORS ####################
+public:
+  /**
+   * \brief Constructs a frame buffer with the specified dimensions.
+   *
+   * \param width   The width of the frame buffer.
+   * \param height  The height of the frame buffer.
+   */
+  FrameBuffer(int width, int height);
+
+  //#################### DESTRUCTOR ####################
+public:
+  /**
+   * \brief Destroys the frame buffer.
+   */
+  ~FrameBuffer();
+
+  //#################### COPY CONSTRUCTOR & ASSIGNMENT OPERATOR ####################
+private:
+  // Deliberately private and unimplemented.
+  FrameBuffer(const FrameBuffer&);
+  FrameBuffer& operator=(const FrameBuffer&);
+
+  //#################### PUBLIC MEMBER FUNCTIONS ####################
+public:
+  /**
+   * \brief Gets the texture ID of the frame buffer's colour buffer.
+   *
+   * \return  The texture ID of the frame buffer's colour buffer.
+   */
+  GLuint get_colour_buffer_id() const;
+
+  /**
+   * \brief Makes the frame buffer the current rendering target.
+   */
+  void make_active();
+};
+
+}
+
+#endif
---
+++ modules/spaint/src/ogl/FrameBuffer.cpp
@@ -0,0 +1,64 @@
+/**
+ * spaint: FrameBuffer.cpp
+ */
+
+#include "ogl/FrameBuffer.h"
+
+#include <cstddef>
+
+namespace spaint {
+
+//#################### CONSTRUCTORS ####################
+
+FrameBuffer::FrameBuffer(int width, int height)
+{
+  // Set up the colour buffer.
+  glGenTextures(1, &m_colourBufferID);
+  glBindTexture(GL_TEXTURE_2D, m_colourBufferID);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+  // Set up the depth buffer.
+  glGenRenderbuffers(1, &m_depthBufferID);
+  glBindRenderbuffer(GL_RENDERBUFFER, m_depthBufferID);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
+
+  // Set up the frame buffer.
+  glGenFramebuffers(1, &m_id);
+  glBindFramebuffer(GL_FRAMEBUFFER, m_id);
+
+  // Attach the colour buffer to the frame buffer.
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colourBufferID, 0);
+  GLenum drawBuffer = GL_COLOR_ATTACHMENT0;
+  glDrawBuffers(1, &drawBuffer);
+
+  // Attach the depth buffer to the frame buffer.
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depthBufferID);
+
+  // Switch back to rendering to the normal display.
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+//#################### DESTRUCTOR ####################
+
+FrameBuffer::~FrameBuffer()
+{
+  glDeleteRenderbuffers(1, &m_depthBufferID);
+  glDeleteTextures(1, &m_colourBufferID);
+  glDeleteFramebuffers(1, &m_id);
+}
+
+//#################### PUBLIC MEMBER FUNCTIONS ####################
+
+GLuint FrameBuffer::get_colour_buffer_id() const
+{
+  return m_colourBufferID;
+}
+
+void FrameBuffer::make_active()
+{
+  glBindFramebuffer(GL_FRAMEBUFFER, m_id);
+}
+
+}
