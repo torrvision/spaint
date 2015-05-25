8d6094934a05bc613931b6e7aaedf6a1784c3973
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-05-25 15:57:33 +0100

    Make a few minor improvements to FrameBuffer.

--- modules/spaint/src/ogl/FrameBuffer.cpp
+++ modules/spaint/src/ogl/FrameBuffer.cpp
@@ -16,8 +16,8 @@ FrameBuffer::FrameBuffer(int width, int height)
   glGenTextures(1, &m_colourBufferID);
   glBindTexture(GL_TEXTURE_2D, m_colourBufferID);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
   // Set up the depth buffer.
   glGenRenderbuffers(1, &m_depthBufferID);
@@ -30,8 +30,6 @@ FrameBuffer::FrameBuffer(int width, int height)
 
   // Attach the colour buffer to the frame buffer.
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colourBufferID, 0);
-  GLenum drawBuffer = GL_COLOR_ATTACHMENT0;
-  glDrawBuffers(1, &drawBuffer);
 
   // Attach the depth buffer to the frame buffer.
   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depthBufferID);
