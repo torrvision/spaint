6f4a3f1c107857bc3a69edf04670a6e5e830e0fa
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-05-26 08:59:51 +0100

    Add the .tgz file and build script for GLEW.

---
+++ libraries/build-glew-1.12.0-win.sh
@@ -0,0 +1,29 @@
+#! /bin/bash -e
+
+LOG=../../build-glew-1.12.0.log
+
+# Build glew.
+echo "[spaint] Building glew 1.12.0"
+
+if [ -d glew-1.12.0 ]
+then
+  echo "[spaint] ...Skipping archive extraction (already extracted)"
+else
+  echo "[spaint] ...Extracting archive..."
+  /bin/rm -fR tmp
+  mkdir tmp
+  cd tmp
+  tar xzf ../setup/glew-1.12.0/glew-1.12.0.tgz
+  cd ..
+  mv tmp/glew-1.12.0 .
+  rmdir tmp
+fi
+
+cd glew-1.12.0/build/vc12
+
+echo "[spaint] ...Running build..."
+cmd //c "msbuild /p:Configuration=Release glew.sln >> $LOG 2>&1"
+
+cd ..
+
+echo "[spaint] ...Finished building glew-1.12.0."
