22ecc32339d99402659ce6f138914f56bfe2b52b
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-06-02 16:49:49 +0100

    Tidy up DepthVisualiser_Shared.h.

--- modules/spaint/include/spaint/visualisers/shared/DepthVisualiser_Shared.h
+++ modules/spaint/include/spaint/visualisers/shared/DepthVisualiser_Shared.h
@@ -5,7 +5,7 @@
 #ifndef H_SPAINT_DEPTHVISUALISER_SHARED
 #define H_SPAINT_DEPTHVISUALISER_SHARED
 
-#include <cstdlib>
+#include <cmath>
 
 #include <ITMLib/Engine/DeviceAgnostic/ITMRepresentationAccess.h>
 #include <ITMLib/Engine/DeviceAgnostic/ITMVisualisationEngine.h>
@@ -15,29 +15,33 @@ namespace spaint {
 //#################### SHARED HELPER FUNCTIONS ####################
 
 /**
- * \brief Computes the necessart depth of a pixel from the camera plane.
+ * \brief Computes the depth of a point in the scene relative to a known camera location.
  *
- * \param dest        A location into which to write the computed colour.
- * \param point       The location of the point (if any) on the scene surface that was hit by a ray passing from the camera through the pixel.
- * \param foundPoint  A flag indicating whether of not any point was actually hit by the ray (true if yes; false if no).
+ * \param dest              A location into which to write the computed depth.
+ * \param point             The location of the point (if any) on the scene surface that was hit by a ray passing from the camera through the pixel.
+ * \param foundPoint        A flag indicating whether or not any point was actually hit by the ray (true if yes; false if no).
+ * \param cameraPosition    The camera position (in world space).
+ * \param cameraLookVector  The camera look vector.
+ * \param voxelSize         The size of an InfiniTAM voxel (in metres).
+ * \param depthType         The type of depth calculation to use.
  */
 _CPU_AND_GPU_CODE_
-inline void shade_pixel_depth(float& dest, const Vector3f& cameraPoint, const Vector3f& cameraLookVector, const Vector3f& worldPoint, float voxelSize, bool foundPoint, DepthVisualiser::DepthType depthType)
+inline void shade_pixel_depth(float& dest, const Vector3f& point, bool foundPoint, const Vector3f& cameraPosition, const Vector3f& cameraLookVector,
+                              float voxelSize, DepthVisualiser::DepthType depthType)
 {
   dest = -1.0f;
   if(foundPoint)
   {
-    if(depthType == DepthVisualiser::DT_ORTHOGRAPHIC)
+    if(depthType == DepthVisualiser::DT_EUCLIDEAN)
     {
-      dest = (ORUtils::dot(worldPoint, cameraLookVector) - ORUtils::dot(cameraPoint, cameraLookVector));
-    }
-    else if(depthType == DepthVisualiser::DT_EUCLIDEAN)
-    {
-      float dx = abs(cameraPoint.x - worldPoint.x);
-      float dy = abs(cameraPoint.y - worldPoint.y);
-      float dz = abs(cameraPoint.z - worldPoint.z);
-
+      float dx = abs(cameraPosition.x - point.x);
+      float dy = abs(cameraPosition.y - point.y);
+      float dz = abs(cameraPosition.z - point.z);
       dest = sqrt(dx * dx + dy * dy + dz * dz);
+    }
+    else if(depthType == DepthVisualiser::DT_ORTHOGRAPHIC)
+    {
+      dest = ORUtils::dot(point, cameraLookVector) - ORUtils::dot(cameraPosition, cameraLookVector);
     }
   }
 }
