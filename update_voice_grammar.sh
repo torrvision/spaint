d277a05f96c70903837953ca66f6c1fc21c9de67
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-07-06 17:54:50 +0100

    Tidy up an error message.

--- update_voice_grammar.sh
+++ update_voice_grammar.sh
@@ -5,6 +5,6 @@ then
   cd apps/spaintgui/resources
   (cat spaint-pre.gram; (cat Labels.txt | perl -pe 's/^(.*)\r/  label \1 \|/' | sed '$d'); echo "  label `tail -n1 Labels.txt`;") > ../../../../voicecommander/resources/spaint.gram
 else
-  echo "Error: Cannot find the ../voicecommander directory!"
+  echo "Error: Cannot find the voicecommander directory!"
   exit
 fi
