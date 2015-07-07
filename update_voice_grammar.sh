21aa06866192c00ab7a254160615b008b1f0a4fb
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-07-07 12:05:35 +0100

    Continue sorting out update_voice_grammar.sh.

--- update_voice_grammar.sh
+++ update_voice_grammar.sh
@@ -3,7 +3,7 @@
 if [ -d ../voicecommander ]
 then
   cd apps/spaintgui/resources
-  (cat spaint-pre.gram; (cat Labels.txt | perl -pe 's/^(.*)(\r)?/  label \1 \|/' | sed '$d'); echo "  label `tail -n1 Labels.txt`;") > ../../../../voicecommander/resources/spaint.gram
+  (cat spaint-pre.gram; (cat Labels.txt | perl -pe 's/(.*?)(\r|\r\n)/  label \1 \|/' | sed '$d'); echo "  label `tail -n1 Labels.txt`;") > ../../../../voicecommander/resources/spaint.gram
 else
   echo "Error: Cannot find the voicecommander directory!"
   exit
