7f0b5bda48a9b8cb02202cc5fed545d41909acf2
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-07-14 11:17:04 +0100

    Prevent crashes.

--- modules/spaint/src/imageprocessing/interface/ImageProcessor.cpp
+++ modules/spaint/src/imageprocessing/interface/ImageProcessor.cpp
@@ -18,11 +18,16 @@ Vector2i ImageProcessor::image_size(const AFArray_CPtr& img)
 }
 
 void ImageProcessor::median_filter(const ITMUChar4Image_CPtr& inputImage, const ITMUChar4Image_Ptr& outputImage) const
+try
 {
   AFArray_Ptr afImage(new af::array(inputImage->noDims.y, inputImage->noDims.x, 4, u8));
   copy_itm_to_af(inputImage, afImage);
-  *afImage = af::medfilt(*afImage, 7, 7);
+  *afImage = af::medfilt(*afImage, 5, 5);
   copy_af_to_itm(afImage, outputImage);
+}
+catch(af::exception&)
+{
+  // Prevent crashes when median filtering fails.
 }
 
 }
