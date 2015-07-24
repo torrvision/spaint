654f5c81794211ad4c6afa1277b735062f358d54
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-07-24 10:48:03 +0100

    Factor the RGB to CIELab colour conversion into a separate function.

--- modules/spaint/include/spaint/features/shared/VOPFeatureCalculator_Shared.h
+++ modules/spaint/include/spaint/features/shared/VOPFeatureCalculator_Shared.h
@@ -22,6 +22,41 @@ inline float rgb_to_lab_f(float t)
 }
 
 /**
+ * \brief Converts an RGB colour to one in the CIELab colour space.
+ *
+ * \param rgb The RGB colour.
+ * \return    The result of converting the colour to CIELab.
+ */
+_CPU_AND_GPU_CODE_
+inline Vector3f rgb_to_lab(const Vector3f& rgb)
+{
+  // Equivalent Matlab code can be found at: https://www.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/code/Util/RGB2Lab.m
+  // See also: http://docs.opencv.org/modules/imgproc/doc/miscellaneous_transformations.html
+  const float EPSILON = 0.000001f;
+
+  float x = 0.412453f * rgb.r + 0.357580f * rgb.g + 0.180423f * rgb.b;
+  float y = 0.212671f * rgb.r + 0.715160f * rgb.g + 0.072169f * rgb.b;
+  float z = 0.019334f * rgb.r + 0.119193f * rgb.g + 0.950227f * rgb.b;
+
+  x /= 0.950456f;
+  z /= 1.088754f;
+
+  float fx = rgb_to_lab_f(x);
+  float fy = rgb_to_lab_f(y);
+  float fz = rgb_to_lab_f(z);
+
+  float L = y > 0.008856f ? (116.0f * fy - 16.0f) : (903.3f * y);
+  float A = 500.0f * (fx - fy);
+  float B = 200.0f * (fy - fz);
+
+  float AplusB = fabs(A + B) + EPSILON;
+  A /= AplusB;
+  B /= AplusB;
+
+  return Vector3f(L, A, B);
+}
+
+/**
  * \brief Converts the RGB patch for the specified voxel to the CIELab colour space.
  *
  * The patches are stored as the patch segments of the feature descriptors for the various voxels.
@@ -33,37 +68,15 @@ inline float rgb_to_lab_f(float t)
 _CPU_AND_GPU_CODE_
 inline void convert_patch_to_lab(int voxelLocationIndex, size_t featureCount, float *features)
 {
-  const float EPSILON = 0.000001f;
-
   // Convert each RGB colour in the patch segment of the voxel's feature vector to the CIELab colour space.
   for(size_t i = voxelLocationIndex * featureCount, end = i + featureCount - 4; i != end; i += 3)
   {
-    // Equivalent Matlab code can be found at: https://www.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/code/Util/RGB2Lab.m
-    // See also: http://docs.opencv.org/modules/imgproc/doc/miscellaneous_transformations.html
-    float r = features[i] / 255.0f;
-    float g = features[i+1] / 255.0f;
-    float b = features[i+2] / 255.0f;
+    Vector3f rgb(features[i] / 255.0f, features[i+1] / 255.0f, features[i+2] / 255.0f);
+    Vector3f lab = rgb_to_lab(rgb);
 
-    float x = 0.412453f * r + 0.357580f * g + 0.180423f * b;
-    float y = 0.212671f * r + 0.715160f * g + 0.072169f * b;
-    float z = 0.019334f * r + 0.119193f * g + 0.950227f * b;
-
-    x /= 0.950456f;
-    z /= 1.088754f;
-
-    float fx = rgb_to_lab_f(x);
-    float fy = rgb_to_lab_f(y);
-    float fz = rgb_to_lab_f(z);
-
-    float L = y > 0.008856f ? (116.0f * fy - 16.0f) : (903.3f * y);
-    float A = 500.0f * (fx - fy);
-    float B = 200.0f * (fy - fz);
-
-    float AplusB = fabs(A + B) + EPSILON;
-
-    features[i] = L;
-    features[i+1] = A / AplusB;
-    features[i+2] = B / AplusB;
+    features[i] = lab.x;
+    features[i+1] = lab.y;
+    features[i+2] = lab.z;
   }
 }
 
