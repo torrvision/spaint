fbc70e082d4ac9ce0c20198e1bf0c80e4bf2e53c
mikesapi <mikesapi.code.email@gmail.com>
2016-02-15 15:00:11 +0000

    tidying up attitude util

--- modules/tvgutil/include/tvgutil/AttitudeUtil.h
+++ modules/tvgutil/include/tvgutil/AttitudeUtil.h
@@ -14,153 +14,40 @@
 namespace tvgutil {
 
 /**
- * \brief This struct may be used to convert between various 3D attitude representations.
+ * \brief This class may be used to convert between various 3D attitude representations.
+ *
+ * Note: The conversions were taken from "James Diebel. Representing Attitude: Euler Angles, Quaternions, and Rotation Vectors. Technical Report, Stanford University, Palo Alto, CA."
  */
-struct AttitudeUtil
+class AttitudeUtil
 {
   //#################### PUBLIC STATIC MEMBER FUNCTIONS ####################
 public:
   /**
-   * \brief Calculates the L2 norm of a vector.
+   * \brief Converts from an axis-angle representation to a rotation matrix.
    *
-   * \param v  The vector.
-   * \return   The L2 norm of the vector.
+   * \param axis   The axis of rotation.
+   * \param angle  The angle of rotation.
+   * \param matrix The rotation matrix.
    */
   template <typename T>
-  static T l2_norm(const T *v, size_t elementCount)
+  static void axis_angle_to_rotation_matrix(const T *axis, const T *angle, T *matrix)
   {
-    T sumSquares = 0.0;
-    for(size_t i = 0; i < elementCount; ++i)
-    {
-      sumSquares += v[i] * v[i];
-    }
-    return sqrt(sumSquares);
-  }
+    float rv[3];
+    axis_angle_to_rotation_vector(axis, angle, rv);
 
-  template <typename T>
-  static T l2_norm(const std::vector<T>& v)
-  {
-    return l2_norm(&v.front(), v.size());
+    float q[4];
+    rotation_vector_to_quaternion(rv, q);
+
+    quaternion_to_rotation_matrix(q, matrix);
   }
 
   /**
-   * \brief Converts a rotation matrix to a quaternion.
+   * \brief Converts from an axis-angle representation to a rotation vector.
    *
-   * \param matrix   The rotation matrix in row-major format.
-   * \param q        The quaternion.
+   * \param axis  The axis of rotation.
+   * \param angle The angle of rotation.
+   * \param rv    The rotation vector.
    */
-  template <typename T>
-  static void rotation_matrix_to_quaternion(const T *matrix, T *q)
-  {
-    /* Taken from "James Diebel. Representing Attitude: Euler Angles, Quaternions, and Rotation Vectors. Technical Report, Stanford University, Palo Alto, CA." */
-
-    int variant = 0;
-    if((matrix[4]>-matrix[8])&&(matrix[0]>-matrix[4])&&(matrix[0]>-matrix[8]))
-    {
-      variant=0;
-    }
-    else if((matrix[4]<-matrix[8])&&(matrix[0]>matrix[4])&&(matrix[0]> matrix[8]))
-    {
-      variant=1;
-    } else if((matrix[4]> matrix[8])&&(matrix[0]<matrix[4])&&(matrix[0]<-matrix[8]))
-    {
-      variant=2;
-    } else if((matrix[4]<matrix[8])&&(matrix[0]<-matrix[4])&&(matrix[0]< matrix[8]))
-    {
-      variant=3;
-    }
-
-    // choose the numerically best variant...
-    double denom = 1.0;
-    if (variant==0)
-    {
-      denom+=matrix[0]+matrix[4]+matrix[8];
-    } else
-    {
-      int tmp = variant*4;
-      denom+=matrix[tmp-4];
-      denom-=matrix[tmp%12];
-      denom-=matrix[(tmp+4)%12];
-    }
-    denom=sqrt(denom);
-    q[variant] = 0.5*denom;
-
-    denom *= 2.0;
-    switch(variant) {
-    case 0:
-            q[1] = (matrix[5] - matrix[7])/denom;
-            q[2] = (matrix[6] - matrix[2])/denom;
-            q[3] = (matrix[1] - matrix[3])/denom;
-            break;
-    case 1:
-            q[0] = (matrix[5] - matrix[7])/denom;
-            q[2] = (matrix[1] + matrix[3])/denom;
-            q[3] = (matrix[6] + matrix[2])/denom;
-            break;
-    case 2:
-            q[0] = (matrix[6] - matrix[2])/denom;
-            q[1] = (matrix[1] + matrix[3])/denom;
-            q[3] = (matrix[5] + matrix[7])/denom;
-            break;
-    case 3:
-            q[0] = (matrix[1] - matrix[3])/denom;
-            q[1] = (matrix[6] + matrix[2])/denom;
-            q[2] = (matrix[5] + matrix[7])/denom;
-            break;
-    }
-    //q[0]*=-1.0; // TODO: I think this is a bug.
-  }
-
-  /**
-   * \brief Converts a quaternion to a rotation matrix.
-   *
-   * \param q      The quaternion, where q[0] is the real part and q[1-3] are the imaginary parts.
-   * \param matrix The rotation matrix in row-major format.
-   */
-  template <typename T>
-  static void quaternion_to_rotation_matrix(const T *q, T *matrix)
-  {
-          matrix[0] = q[0]*q[0] + q[1]*q[1] - q[2]*q[2] - q[3]*q[3];
-          matrix[4] = q[0]*q[0] - q[1]*q[1] + q[2]*q[2] - q[3]*q[3];
-          matrix[8] = q[0]*q[0] - q[1]*q[1] - q[2]*q[2] + q[3]*q[3];
-          matrix[3] = 2*(q[1]*q[2] - q[0]*q[3]);
-          matrix[1] = 2*(q[1]*q[2] + q[0]*q[3]);
-          matrix[6] = 2*(q[1]*q[3] + q[0]*q[2]);
-          matrix[2] = 2*(q[1]*q[3] - q[0]*q[2]);
-          matrix[7] = 2*(q[2]*q[3] - q[0]*q[1]);
-          matrix[5] = 2*(q[2]*q[3] + q[0]*q[1]);
-  }
-
-  /**
-   * \brief Converts a rotation vector to a quaternion.
-   *
-   * \param r  The rotation vector.
-   * \return   The equivalent quaternion.
-   */
-  template <typename T>
-  static void rotation_vector_to_quaternion(const T *rv, T *q)
-  {
-    float axis[3];
-    float angle;
-    rotation_vector_to_axis_angle(rv, axis, &angle);
-
-    // Create the quaternion.
-    T sinHalfTheta = sin(angle/2.0f);
-    q[0] = cos(angle/2.0f);
-    q[1] = axis[0] * sinHalfTheta;
-    q[2] = axis[1] * sinHalfTheta;
-    q[3] = axis[2] * sinHalfTheta;
-  }
-
-  template <typename T>
-  static void quaternion_to_rotation_vector(const T *q, T *rv)
-  {
-    T axis[3];
-    T angle;
-    quaternion_to_axis_angle(q, axis, &angle);
-    axis_angle_to_rotation_vector(axis, &angle, rv);
-  }
-
   template <typename T>
   static void axis_angle_to_rotation_vector(const T *axis, const T *angle, T *rv)
   {
@@ -169,6 +56,13 @@ public:
     rv[2] = axis[2] * *angle;
   }
 
+  /**
+   * \brief Converts from a unit quaternion to an axis-angle representation.
+   *
+   * \param q       The unit quaternion.
+   * \param axis    The axis of the rotation.
+   * \param angle   The angle of the rotation.
+   */
   template <typename T>
   static void quaternion_to_axis_angle(const T *q, T *axis, T *angle)
   {
@@ -196,6 +90,48 @@ public:
     }
   }
 
+  /**
+   * \brief Converts a unit quaternion to a rotation matrix.
+   *
+   * \param q      The unit quaternion, where q[0] is the real part and q[1-3] are the imaginary parts.
+   * \param matrix The rotation matrix in row-major format.
+   */
+  template <typename T>
+  static void quaternion_to_rotation_matrix(const T *q, T *matrix)
+  {
+          matrix[0] = q[0]*q[0] + q[1]*q[1] - q[2]*q[2] - q[3]*q[3];
+          matrix[4] = q[0]*q[0] - q[1]*q[1] + q[2]*q[2] - q[3]*q[3];
+          matrix[8] = q[0]*q[0] - q[1]*q[1] - q[2]*q[2] + q[3]*q[3];
+          matrix[3] = 2*(q[1]*q[2] - q[0]*q[3]);
+          matrix[1] = 2*(q[1]*q[2] + q[0]*q[3]);
+          matrix[6] = 2*(q[1]*q[3] + q[0]*q[2]);
+          matrix[2] = 2*(q[1]*q[3] - q[0]*q[2]);
+          matrix[7] = 2*(q[2]*q[3] - q[0]*q[1]);
+          matrix[5] = 2*(q[2]*q[3] + q[0]*q[1]);
+  }
+
+  /**
+   * \brief Converts a unit quaternion to a rotation vector.
+   *
+   * \param q  The unit quaternion.
+   * \param rv The rotation vector.
+   */
+  template <typename T>
+  static void quaternion_to_rotation_vector(const T *q, T *rv)
+  {
+    T axis[3];
+    T angle;
+    quaternion_to_axis_angle(q, axis, &angle);
+    axis_angle_to_rotation_vector(axis, &angle, rv);
+  }
+
+  /**
+   * \brief Converts a rotation matrix to an axis-angle representation.
+   *
+   * \param matrix  The rotation matrix.
+   * \param axis    The axis of the rotation.
+   * \param angle   The angle of the rotation.
+   */
   template <typename T>
   static void rotation_matrix_to_axis_angle(const T *matrix, T *axis, T *angle)
   {
@@ -204,6 +140,68 @@ public:
     quaternion_to_axis_angle(q, axis, angle);
   }
 
+  /**
+   * \brief Converts a rotation matrix to a quaternion.
+   *
+   * \param matrix   The rotation matrix in row-major format.
+   * \param q        The unit quaternion.
+   */
+  template <typename T>
+  static void rotation_matrix_to_quaternion(const T *matrix, T *q)
+  {
+    int variant = 0;
+
+    if(     (matrix[4] > -matrix[8]) && (matrix[0] > -matrix[4]) && (matrix[0] > -matrix[8])) variant = 0;
+    else if((matrix[4] < -matrix[8]) && (matrix[0] >  matrix[4]) && (matrix[0] >  matrix[8])) variant = 1;
+    else if((matrix[4] >  matrix[8]) && (matrix[0] <  matrix[4]) && (matrix[0] < -matrix[8])) variant = 2;
+    else if((matrix[4] <  matrix[8]) && (matrix[0] < -matrix[4]) && (matrix[0] <  matrix[8])) variant = 3;
+
+    double denominator;
+
+    // Choose the best variant.
+    switch(variant)
+    {
+      case 0:
+        denominator =  2.0f * sqrt(1.0f + matrix[0] + matrix[4] + matrix[8]);
+        q[0] = denominator / 4.0f;
+        q[1] = (matrix[5] - matrix[7]) / denominator;
+        q[2] = (matrix[6] - matrix[2]) / denominator;
+        q[3] = (matrix[1] - matrix[3]) / denominator;
+        break;
+
+      case 1:
+        denominator =  2.0f * sqrt(1.0f + matrix[0] - matrix[4] - matrix[8]);
+        q[0] = (matrix[5] - matrix[7]) / denominator;
+        q[1] = denominator / 4.0f;
+        q[2] = (matrix[1] + matrix[3]) / denominator;
+        q[3] = (matrix[6] + matrix[2]) / denominator;
+        break;
+
+      case 2:
+        denominator = 2.0f * sqrt(1.0f - matrix[0] + matrix[4] - matrix[8]);
+        q[0] = (matrix[6] - matrix[2]) / denominator;
+        q[1] = (matrix[1] + matrix[3]) / denominator;
+        q[2] = denominator / 4.0f;
+        q[3] = (matrix[5] + matrix[7]) / denominator;
+        break;
+
+      case 3:
+        denominator = 2.0f * sqrt(1.0f - matrix[0] - matrix[4] + matrix[8]);
+        q[0] = (matrix[1] - matrix[3]) / denominator;
+        q[1] = (matrix[6] + matrix[2]) / denominator;
+        q[2] = (matrix[5] + matrix[7]) / denominator;
+        q[3] = denominator / 4.0f;
+        break;
+    }
+  }
+
+  /**
+   * \brief Converts a rotation vector to an axis-angle representation.
+   *
+   * \param rv    The rotation vector.
+   * \param axis  The axis of rotation.
+   * \param angle The angle of rotation.
+   */
   template <typename T>
   static void rotation_vector_to_axis_angle(const T *rv, T *axis, T *angle)
   {
@@ -222,16 +220,50 @@ public:
     }
   }
 
+  /**
+   * \brief Converts a rotation vector to a quaternion.
+   *
+   * \param rv  The rotation vector.
+   * \param q   The unit quaternion.
+   */
   template <typename T>
-  static void axis_angle_to_rotation_matrix(const T *axis, const T *angle, T *matrix)
+  static void rotation_vector_to_quaternion(const T *rv, T *q)
   {
-    float rv[3];
-    axis_angle_to_rotation_vector(axis, angle, rv);
+    float axis[3];
+    float angle;
+    rotation_vector_to_axis_angle(rv, axis, &angle);
 
-    float q[4];
-    rotation_vector_to_quaternion(rv, q);
+    // Create the quaternion.
+    T sinHalfTheta = sin(angle/2.0f);
+    q[0] = cos(angle/2.0f);
+    q[1] = axis[0] * sinHalfTheta;
+    q[2] = axis[1] * sinHalfTheta;
+    q[3] = axis[2] * sinHalfTheta;
+  }
 
-    quaternion_to_rotation_matrix(q, matrix);
+  //#################### PRIVATE STATIC MEMBER FUNCTIONS ####################
+private:
+  /**
+   * \brief Calculates the L2 norm of a vector.
+   *
+   * \param v  The vector.
+   * \return   The L2 norm of the vector.
+   */
+  template <typename T>
+  static T l2_norm(const T *v, size_t elementCount)
+  {
+    T sumSquares = 0.0;
+    for(size_t i = 0; i < elementCount; ++i)
+    {
+      sumSquares += v[i] * v[i];
+    }
+    return sqrt(sumSquares);
+  }
+
+  template <typename T>
+  static T l2_norm(const std::vector<T>& v)
+  {
+    return l2_norm(&v.front(), v.size());
   }
 };
 
