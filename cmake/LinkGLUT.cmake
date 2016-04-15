1dcaa300d1fef9bfcdd1aaa91862d6b211dbeea9
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-04-15 17:41:27 +0100

    Fix LinkGLUT.cmake.

--- cmake/LinkGLUT.cmake
+++ cmake/LinkGLUT.cmake
@@ -2,8 +2,10 @@
 # LinkGLUT.cmake #
 ##################
 
-TARGET_LINK_LIBRARIES(${targetname} ${GLUT_LIBRARY})
+IF(WITH_GLUT)
+  TARGET_LINK_LIBRARIES(${targetname} ${GLUT_LIBRARY})
 
-IF(MSVC_IDE)
-  ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${GLUT_ROOT}/bin/x64/freeglut.dll" "$<TARGET_FILE_DIR:${targetname}>")
+  IF(MSVC_IDE)
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${GLUT_ROOT}/bin/x64/freeglut.dll" "$<TARGET_FILE_DIR:${targetname}>")
+  ENDIF()
 ENDIF()
