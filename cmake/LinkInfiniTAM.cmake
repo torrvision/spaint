152dc7cd9f13523eeb8a92f5b834d0691b1ed721
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-06-16 12:51:42 +0100

    Temporarily revert the changes to the InfiniTAM CMake scripts.

--- cmake/LinkInfiniTAM.cmake
+++ cmake/LinkInfiniTAM.cmake
@@ -2,7 +2,7 @@
 # LinkInfiniTAM.cmake #
 #######################
 
-TARGET_LINK_LIBRARIES(${targetname} ${InfiniTAM_LIBRARIES})
+TARGET_LINK_LIBRARIES(${targetname} InputSource ITMLib ORUtils RelocLib)
 
 IF(PNG_FOUND)
   TARGET_LINK_LIBRARIES(${targetname} ${PNG_LIBRARIES})
