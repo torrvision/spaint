012f8cced21cf9517f787ee477eaf6211a3aadbe
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-07-21 14:09:30 +0100

    Start introducing an ObjectSegmenter class.

---
+++ modules/spaint/include/spaint/segmentation/ObjectSegmenter.h
@@ -0,0 +1,21 @@
+/**
+ * spaint: ObjectSegmenter.h
+ * Copyright (c) Torr Vision Group, University of Oxford, 2016. All rights reserved.
+ */
+
+#ifndef H_SPAINT_OBJECTSEGMENTER
+#define H_SPAINT_OBJECTSEGMENTER
+
+namespace spaint {
+
+/**
+ * \brief TODO
+ */
+class ObjectSegmenter
+{
+  // TODO
+};
+
+}
+
+#endif
---
+++ modules/spaint/src/segmentation/ObjectSegmenter.cpp
@@ -0,0 +1,12 @@
+/**
+ * spaint: ObjectSegmenter.cpp
+ * Copyright (c) Torr Vision Group, University of Oxford, 2016. All rights reserved.
+ */
+
+#include "segmentation/ObjectSegmenter.h"
+
+namespace spaint {
+
+// TODO
+
+}
