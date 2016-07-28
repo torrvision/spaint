264b485c6009b9caa6bed9cae7d04f3b1b3f7f6b
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-07-28 14:05:00 +0100

    Templatise SegmentationUtil::apply_mask.

--- modules/spaint/include/spaint/segmentation/SegmentationUtil.h
+++ modules/spaint/include/spaint/segmentation/SegmentationUtil.h
@@ -28,11 +28,46 @@ public:
   /**
    * \brief Applies a binary mask to an image.
    *
+   * This version of the function is needed to assist the compiler with type inference.
+   *
    * \param mask  The binary mask.
    * \param image The image to which to apply it.
    * \return      A masked version of the input image.
    */
-  static ITMUChar4Image_Ptr apply_mask(const ITMUCharImage_CPtr& mask, const ITMUChar4Image_CPtr& image);
+  template <typename T>
+  static boost::shared_ptr<ORUtils::Image<T> > apply_mask(const ITMUCharImage_CPtr& mask, const boost::shared_ptr<ORUtils::Image<T> >& image)
+  {
+    return apply_mask(mask, boost::shared_ptr<const ORUtils::Image<T> >(image));
+  }
+
+  /**
+   * \brief Applies a binary mask to an image.
+   *
+   * \param mask  The binary mask.
+   * \param image The image to which to apply it.
+   * \return      A masked version of the input image.
+   */
+  template <typename T>
+  static boost::shared_ptr<ORUtils::Image<T> > apply_mask(const ITMUCharImage_CPtr& mask, const boost::shared_ptr<const ORUtils::Image<T> >& image)
+  {
+    boost::shared_ptr<ORUtils::Image<T> > maskedImage(new ORUtils::Image<T>(image->noDims, true, false));
+
+    const uchar *maskPtr = mask->GetData(MEMORYDEVICE_CPU);
+    const T *imagePtr = image->GetData(MEMORYDEVICE_CPU);
+    T *maskedImagePtr = maskedImage->GetData(MEMORYDEVICE_CPU);
+
+    int pixelCount = static_cast<int>(image->dataSize);
+
+  #ifdef WITH_OPENMP
+    #pragma omp parallel for
+  #endif
+    for(int i = 0; i < pixelCount; ++i)
+    {
+      maskedImagePtr[i] = maskPtr[i] ? imagePtr[i] : T((uchar)0);
+    }
+
+    return maskedImage;
+  }
 };
 
 }
