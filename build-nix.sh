2bcc2fc182053328ef2e55343e950aa6f9f95ebd
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2016-08-09 10:42:18 +0100

    Handle Eclipse CDT CMake generator

--- build-nix.sh
+++ build-nix.sh
@@ -1,9 +1,9 @@
 #! /bin/bash -e
 
 # Check that a valid build type has been specified.
-if [ $# -ne 2 ] || ([ "$1" != "Unix Makefiles" ] && [ "$1" != "Xcode" ]) || ([ $2 != "Debug" ] && [ $2 != "Release" ])
+if [ $# -ne 2 ] || ([ "$1" != "Unix Makefiles" ] && [ "$1" != "Eclipse CDT4 - Unix Makefiles" ] && [ "$1" != "Xcode" ]) || ([ $2 != "Debug" ] && [ $2 != "Release" ] && [ $2 != "RelWithDebInfo" ])
 then
-  echo "Usage: build-nix.sh {Unix Makefiles|Xcode} {Debug|Release}"
+  echo "Usage: build-nix.sh {Unix Makefiles|Eclipse CDT4 - Unix Makefiles|Xcode} {Debug|Release|RelWithDebInfo}"
   exit
 fi
 
@@ -31,22 +31,31 @@ fi
 cd ..
 
 # Build spaint itself.
-echo "[spaint] Building spaint"
-
-if [ ! -d build ]
+source_dir="$(pwd)"
+build_dir=./build
+if [ "$1" == "Eclipse CDT4 - Unix Makefiles" ]
 then
-  mkdir build
-  cd build
+  # Eclipse does not like having the build folder inside the project
+  # Use a sibling folder instead
+  build_dir=../spaint-build
+fi
+
+echo "[spaint] Building spaint in $build_dir"
+
+if [ ! -d $build_dir ]
+then
+  mkdir $build_dir
+  cd $build_dir
 
   # Note: We need to configure twice to handle conditional building.
   echo "[spaint] ...Configuring using CMake..."
-  cmake -G"$1" -DCMAKE_BUILD_TYPE=$2 ..
-  cmake ..
+  cmake -G"$1" -DCMAKE_BUILD_TYPE=$2 $source_dir
+  cmake $source_dir
 
-  cd ..
+  cd $source_dir
 fi
 
-cd build
+cd $build_dir
 
 echo "[spaint] ...Running build..."
 make -j2
