403579ebf5057c27bfd81869212a7d93e19484b5
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-08-11 10:01:26 +0100

    Tidy up a bit.

--- cmake/UseOpenCV.cmake
+++ cmake/UseOpenCV.cmake
@@ -6,8 +6,6 @@ OPTION(WITH_OPENCV "Build with OpenCV support?" OFF)
 
 IF(WITH_OPENCV)
   FIND_PACKAGE(OpenCV 3.1.0 REQUIRED HINTS "${PROJECT_SOURCE_DIR}/libraries/opencv-3.1.0/build")
-
-  # Ensure that OpenCV include directories are prepended to the include path.
   INCLUDE_DIRECTORIES(BEFORE SYSTEM ${OpenCV_INCLUDE_DIRS})
   ADD_DEFINITIONS(-DWITH_OPENCV)
 ENDIF()
