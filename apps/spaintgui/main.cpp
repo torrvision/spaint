f2fcb594ad40027fb1c6089724586f91f5b0bbbc
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-08-24 14:57:12 +0100

    Get rid of the object scene for now.

--- apps/spaintgui/main.cpp
+++ apps/spaintgui/main.cpp
@@ -242,14 +242,6 @@ try
   Pipeline_Ptr pipeline(new Pipeline(settings, Application::resources_dir().string(), labelManager));
   pipeline->add_scene_pipeline("World", imageSourceEngine, seed, trackerType, trackerParams);
 
-#if 1
-  // TEMPORARY
-  boost::shared_ptr<CompositeImageSourceEngine> objectImageSourceEngine(new CompositeImageSourceEngine);
-  ImageMaskPathGenerator objectPathGenerator("C:/fr4_tsukuba/rgb_rnm/%06i.ppm", "C:/fr4_tsukuba/depth_rnm/%06i.pgm");
-  objectImageSourceEngine->addSubengine(new ImageFileReader<ImageMaskPathGenerator>("C:/fr4_tsukuba/calibration.txt", objectPathGenerator, 0));
-  pipeline->add_scene_pipeline("Object", objectImageSourceEngine, seed, trackerType, trackerParams);
-#endif
-
   // Run the application.
   Application app(pipeline);
   app.run();
