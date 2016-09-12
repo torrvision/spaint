791d56ac0323698676bf17805e59ca51dfe4afbc
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2016-09-12 14:52:11 +0100

    tidy up

--- modules/tvgutil/include/tvgutil/misc/ThreadPool.h
+++ modules/tvgutil/include/tvgutil/misc/ThreadPool.h
@@ -9,6 +9,7 @@
 #include <string>
 
 #include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
 
 namespace tvgutil {
@@ -21,30 +22,58 @@ class ThreadPool
 {
   //#################### PUBLIC STATIC MEMBER FUNCTIONS ####################
 public:
+  /**
+   * \brief  Returns a static instance of the ThreadPool constructed with default parameters.
+   *         Can be used when there is no need to determine the lifecycle of the thread pool.
+   *
+   * \return A static instance of the ThreadPool constructed with default parameters.
+   */
   static ThreadPool& instance();
 
-  explicit ThreadPool(size_t num_threads = 20);
-  ~ThreadPool();
-
+  //#################### PUBLIC MEMBER FUNCTIONS ####################
+  /**
+   * \brief Starts a function on a separate thread.
+   *
+   * \param task  The task that will executed on a thread part of the pool.
+   */
   template<typename F>
   void start_asynch(F task)
   {
     m_scheduler.post(task);
   }
 
+  //#################### CONSTRUCTOR ####################
+  /**
+   * \brief Constructs a ThreadPool.
+   *
+   * \param num_threads The number of threads part of the pool.
+   */
+  explicit ThreadPool(size_t num_threads = 20);
+
+  //#################### DESTRUCTOR ####################
+  /**
+   * \brief Destructs a ThreadPool. All threads in the pool are joined.
+   *
+   * \note  Can be blocking.
+   */
+  ~ThreadPool();
+
 private:
-  // Kill copy and assignment operations
+  // Disable copy and assignment operations.
   ThreadPool(const ThreadPool &);
   const ThreadPool& operator=(const ThreadPool &);
 
 
-  //#################### MEMBER VARIABLES ####################
+  //#################### PRIVATE MEMBER VARIABLES ####################
 private:
+  // io_service used to schedule work for the threads.
   boost::asio::io_service m_scheduler;
-  boost::asio::io_service::work m_worker;
 
+  // Contains all the threads part of the pool.
   boost::thread_group m_threadpool;
 
+  // Worker variable used to keep the scheduler running.
+  boost::shared_ptr<boost::asio::io_service::work> m_worker;
 };
 
 }
--- modules/tvgutil/src/misc/ThreadPool.cpp
+++ modules/tvgutil/src/misc/ThreadPool.cpp
@@ -9,25 +9,35 @@
 
 namespace tvgutil {
 
+//#################### PUBLIC STATIC MEMBER FUNCTIONS ####################
 ThreadPool& ThreadPool::instance()
 {
+  // Use a default constructed instance of the ThreadPool.
   static ThreadPool instance;
+
   return instance;
 }
 
-ThreadPool::~ThreadPool()
-{
-  m_scheduler.stop();
-  m_threadpool.join_all();
-}
-
+//#################### CONSTRUCTOR ####################
 ThreadPool::ThreadPool(size_t num_threads) :
-    m_worker(m_scheduler)
+    m_worker(new boost::asio::io_service::work(m_scheduler))
 {
+  // Create num_threads threads all running the run() function of the io_service.
   for (size_t i = 0; i < num_threads; ++i)
   {
     m_threadpool.create_thread(boost::bind(&boost::asio::io_service::run, &m_scheduler));
   }
+}
+
+//#################### DESTRUCTOR ####################
+ThreadPool::~ThreadPool()
+{
+  // Stop executing the io_service::run() function but allow running and queued tasks to finish cleanly.
+  m_worker.reset();
+  // Wait for all threads to terminate.
+  m_threadpool.join_all();
+  //Cleanly stop the io_service
+  m_scheduler.stop();
 }
 
 }
