3662894aed9c341f50bbad89e16d03919ea68415
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-10-07 17:39:27 +0100

    Add the files needed for the fiducial relocaliser.

---
+++ modules/spaint/include/spaint/fiducials/FiducialRelocaliser.h
@@ -0,0 +1,21 @@
+/**
+ * spaint: FiducialRelocaliser.h
+ * Copyright (c) Torr Vision Group, University of Oxford, 2016. All rights reserved.
+ */
+
+#ifndef H_SPAINT_FIDUCIALRELOCALISER
+#define H_SPAINT_FIDUCIALRELOCALISER
+
+namespace spaint {
+
+/**
+ * \brief TODO
+ */
+class FiducialRelocaliser
+{
+  // TODO
+};
+
+}
+
+#endif
---
+++ modules/spaint/src/fiducials/FiducialRelocaliser.cpp
@@ -0,0 +1,12 @@
+/**
+ * spaint: FiducialRelocaliser.cpp
+ * Copyright (c) Torr Vision Group, University of Oxford, 2016. All rights reserved.
+ */
+
+#include "fiducials/FiducialRelocaliser.h"
+
+namespace spaint {
+
+// TODO
+
+}
