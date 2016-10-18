9f5af70cb71f2d24d755b845b1e2ced98c8f473d
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-10-18 11:32:34 +0100

    Tidy up AveragingFiducial a bit.

--- modules/spaint/src/fiducials/AveragingFiducial.cpp
+++ modules/spaint/src/fiducials/AveragingFiducial.cpp
@@ -5,31 +5,10 @@
 
 #include "fiducials/AveragingFiducial.h"
 
-#include <iostream>
-
 #include <ITMLib/Utils/ITMMath.h>
-
-#include <tvgutil/misc/AttitudeUtil.h>
-using namespace tvgutil;
 
 #include "geometry/DualQuaternion.h"
 using namespace spaint;
-
-// TEMPORARY
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const DualNumber<T>& rhs)
-{
-  os << '(' << rhs.r << ',' << rhs.d << ')';
-  return os;
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const DualQuaternion<T>& rhs)
-{
-  os << '[' << rhs.w << ',' << rhs.x << ',' << rhs.y << ',' << rhs.z << ']';
-  return os;
-}
-// END TEMPORARY
 
 namespace spaint {
 
@@ -43,23 +22,19 @@ AveragingFiducial::AveragingFiducial(const std::string& id, const ORUtils::SE3Po
 
 void AveragingFiducial::integrate_sub(const FiducialMeasurement& measurement)
 {
-  // TODO: Comment.
+  // Get the rotation and translation components of both the existing fiducial pose and the measurement pose.
   Vector3f r, t, newR, newT;
   m_pose.GetParams(t, r);
   measurement.pose_world()->GetParams(newT, newR);
 
-  // TODO: Comment.
+  // Convert the two poses to dual quaternions.
   DualQuatf q = DualQuatf::from_translation(t) * DualQuatf::from_rotation(r);
   DualQuatf newQ = DualQuatf::from_translation(newT) * DualQuatf::from_rotation(newR);
 
-  // TODO: Calculate the correct interpolation parameter (use the confidence of the fiducial).
+  // Compute a confidence-weighted average of the two poses using dual quaternion interpolation
+  // and set this as the new pose for the fiducial.
   DualQuatf avgQ = DualQuatf::sclerp(newQ, q, m_confidence / (m_confidence + 1.0f));
-
-  // TODO: Determine avgR and avgT from avgQ.
-  Vector3f avgR = avgQ.get_rotation();
-  Vector3f avgT = avgQ.get_translation();
-
-  m_pose.SetFrom(avgT, avgR);
+  m_pose.SetFrom(avgQ.get_translation(), avgQ.get_rotation());
 }
 
 }
