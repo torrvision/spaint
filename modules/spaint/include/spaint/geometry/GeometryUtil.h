7b831d93ef88d0805d0e04e195dec6b27307d87f
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-10-19 12:23:00 +0100

    Start filling in some comments.

--- modules/spaint/include/spaint/geometry/GeometryUtil.h
+++ modules/spaint/include/spaint/geometry/GeometryUtil.h
@@ -15,14 +15,17 @@
 namespace spaint {
 
 /**
- * \brief TODO
+ * \brief This struct provides a number of useful geometric utility functions.
  */
 struct GeometryUtil
 {
   //#################### PUBLIC STATIC MEMBER FUNCTIONS ####################
 
   /**
-   * \brief TODO
+   * \brief Converts a dual quaternion to an SE(3) pose.
+   *
+   * \param dq  The dual quaternion.
+   * \return    The corresponding SE(3) pose.
    */
   template <typename T>
   static ORUtils::SE3Pose dual_quat_to_pose(const DualQuaternion<T>& dq)
@@ -33,7 +36,10 @@ struct GeometryUtil
   }
 
   /**
-   * \brief TODO
+   * \brief Converts an SE(3) pose to a dual quaternion.
+   *
+   * \param pose  The SE(3) pose.
+   * \return      The corresponding dual quaternion.
    */
   template <typename T>
   static DualQuaternion<T> pose_to_dual_quat(const ORUtils::SE3Pose& pose)
@@ -48,7 +54,18 @@ struct GeometryUtil
   }
 
   /**
-   * \brief TODO
+   * \brief Determines whether or not two SE(3) poses are sufficiently similar.
+   *
+   * Similarity is defined in terms of both the rotations and translations involved. Rotation similarity is
+   * assessed by looking at the relative rotation mapping one of the two input rotations to the other, and
+   * thresholding the angle involved. Translation similarity is assessed by thresholding the distance between
+   * the two input translations. Iff both their rotations and translations are similar, so are the poses.
+   *
+   * \param pose1           The first pose.
+   * \param pose2           The second pose.
+   * \param rotThreshold    The angular threshold to use when comparing the rotations.
+   * \param transThreshold  The distance threshold to use when comparing the translations.
+   * \return                true, if the poses are sufficiently similar, or false otherwise.
    */
   static bool poses_are_similar(const ORUtils::SE3Pose& pose1, const ORUtils::SE3Pose& pose2, double rotThreshold = 20 * M_PI / 180, float transThreshold = 0.05f);
 };
