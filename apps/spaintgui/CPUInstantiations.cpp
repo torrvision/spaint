7ea290a09a64fce5847dae73c0f97375149627ae
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2016-11-18 18:13:30 +0000

    remove RenTracker references

--- apps/spaintgui/CPUInstantiations.cpp
+++ apps/spaintgui/CPUInstantiations.cpp
@@ -8,15 +8,11 @@
 #include <ITMLib/Engines/Reconstruction/CPU/ITMSceneReconstructionEngine_CPU.tpp>
 #include <ITMLib/Engines/Swapping/CPU/ITMSwappingEngine_CPU.tpp>
 #include <ITMLib/Engines/Visualisation/CPU/ITMVisualisationEngine_CPU.tpp>
-#include <ITMLib/Trackers/CPU/ITMRenTracker_CPU.tpp>
-#include <ITMLib/Trackers/Interface/ITMRenTracker.tpp>
 #include <spaint/util/SpaintVoxel.h>
 using namespace spaint;
 
 template class ITMDenseMapper<SpaintVoxel,ITMVoxelIndex>;
 template class ITMMeshingEngine_CPU<SpaintVoxel,ITMVoxelIndex>;
-template class ITMRenTracker<SpaintVoxel,ITMVoxelIndex>;
-template class ITMRenTracker_CPU<SpaintVoxel,ITMVoxelIndex>;
 template class ITMSceneReconstructionEngine_CPU<SpaintVoxel,ITMVoxelIndex>;
 template class ITMSwappingEngine_CPU<SpaintVoxel,ITMVoxelIndex>;
 template class ITMVisualisationEngine_CPU<SpaintVoxel,ITMVoxelIndex>;
