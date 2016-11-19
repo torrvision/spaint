49596737dc5d4269ee873fc5fa29bbf72f8512b6
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-11-19 15:41:55 +0000

    Start adding tests for GeometryUtil.

---
+++ tests/unit/spaint/test_GeometryUtil.cpp
@@ -0,0 +1,26 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <spaint/geometry/GeometryUtil.h>
+using namespace spaint;
+
+//#################### TESTS ####################
+
+BOOST_AUTO_TEST_SUITE(test_GeometryUtil)
+
+BOOST_AUTO_TEST_CASE(test_dual_quat_to_pose)
+{
+  // TODO
+}
+
+BOOST_AUTO_TEST_CASE(test_pose_to_dual_quat)
+{
+  // TODO
+}
+
+BOOST_AUTO_TEST_CASE(test_poses_are_similar)
+{
+  // TODO
+}
+
+BOOST_AUTO_TEST_SUITE_END()
