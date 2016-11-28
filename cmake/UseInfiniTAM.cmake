c08ab136fdbd893418330aa4138d51eb5ee54a3a
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-11-28 12:08:20 +0000

    Continue updating to the new version of InfiniTAM.

--- cmake/UseInfiniTAM.cmake
+++ cmake/UseInfiniTAM.cmake
@@ -7,20 +7,23 @@ SET(InfiniTAM_BUILD_DIR "${PROJECT_SOURCE_DIR}/../InfiniTAM/InfiniTAM/build" CAC
 
 FIND_LIBRARY(InfiniTAM_InputSource_LIBRARY_DEBUG InputSource HINTS "${InfiniTAM_BUILD_DIR}/InputSource" "${InfiniTAM_BUILD_DIR}/InputSource/Debug")
 FIND_LIBRARY(InfiniTAM_ITMLib_LIBRARY_DEBUG ITMLib HINTS "${InfiniTAM_BUILD_DIR}/ITMLib" "${InfiniTAM_BUILD_DIR}/ITMLib/Debug")
+FIND_LIBRARY(InfiniTAM_MiniSlamGraphLib_LIBRARY_DEBUG MiniSlamGraphLib HINTS "${InfiniTAM_BUILD_DIR}/MiniSlamGraphLib" "${InfiniTAM_BUILD_DIR}/MiniSlamGraphLib/Debug")
 FIND_LIBRARY(InfiniTAM_ORUtils_LIBRARY_DEBUG ORUtils HINTS "${InfiniTAM_BUILD_DIR}/ORUtils" "${InfiniTAM_BUILD_DIR}/ORUtils/Debug")
 FIND_LIBRARY(InfiniTAM_RelocLib_LIBRARY_DEBUG RelocLib HINTS "${InfiniTAM_BUILD_DIR}/RelocLib" "${InfiniTAM_BUILD_DIR}/RelocLib/Debug")
 
 FIND_LIBRARY(InfiniTAM_InputSource_LIBRARY_RELEASE InputSource HINTS "${InfiniTAM_BUILD_DIR}/InputSource" "${InfiniTAM_BUILD_DIR}/InputSource/Release")
 FIND_LIBRARY(InfiniTAM_ITMLib_LIBRARY_RELEASE ITMLib HINTS "${InfiniTAM_BUILD_DIR}/ITMLib" "${InfiniTAM_BUILD_DIR}/ITMLib/Release")
+FIND_LIBRARY(InfiniTAM_MiniSlamGraphLib_LIBRARY_RELEASE MiniSlamGraphLib HINTS "${InfiniTAM_BUILD_DIR}/MiniSlamGraphLib" "${InfiniTAM_BUILD_DIR}/MiniSlamGraphLib/Release")
 FIND_LIBRARY(InfiniTAM_ORUtils_LIBRARY_RELEASE ORUtils HINTS "${InfiniTAM_BUILD_DIR}/ORUtils" "${InfiniTAM_BUILD_DIR}/ORUtils/Release")
 FIND_LIBRARY(InfiniTAM_RelocLib_LIBRARY_RELEASE RelocLib HINTS "${InfiniTAM_BUILD_DIR}/RelocLib" "${InfiniTAM_BUILD_DIR}/RelocLib/Release")
 
 SET(InfiniTAM_InputSource_LIBRARY debug ${InfiniTAM_InputSource_LIBRARY_DEBUG} optimized ${InfiniTAM_InputSource_LIBRARY_RELEASE})
 SET(InfiniTAM_ITMLib_LIBRARY debug ${InfiniTAM_ITMLib_LIBRARY_DEBUG} optimized ${InfiniTAM_ITMLib_LIBRARY_RELEASE})
+SET(InfiniTAM_MiniSlamGraphLib_LIBRARY debug ${InfiniTAM_MiniSlamGraphLib_LIBRARY_DEBUG} optimized ${InfiniTAM_MiniSlamGraphLib_LIBRARY_RELEASE})
 SET(InfiniTAM_ORUtils_LIBRARY debug ${InfiniTAM_ORUtils_LIBRARY_DEBUG} optimized ${InfiniTAM_ORUtils_LIBRARY_RELEASE})
 SET(InfiniTAM_RelocLib_LIBRARY debug ${InfiniTAM_RelocLib_LIBRARY_DEBUG} optimized ${InfiniTAM_RelocLib_LIBRARY_RELEASE})
 
-SET(InfiniTAM_LIBRARIES ${InfiniTAM_InputSource_LIBRARY} ${InfiniTAM_ITMLib_LIBRARY} ${InfiniTAM_ORUtils_LIBRARY} ${InfiniTAM_RelocLib_LIBRARY})
+SET(InfiniTAM_LIBRARIES ${InfiniTAM_InputSource_LIBRARY} ${InfiniTAM_ITMLib_LIBRARY} ${InfiniTAM_MiniSlamGraphLib_LIBRARY} ${InfiniTAM_ORUtils_LIBRARY} ${InfiniTAM_RelocLib_LIBRARY})
 
 INCLUDE_DIRECTORIES(${InfiniTAM_INCLUDE_DIR})
 
