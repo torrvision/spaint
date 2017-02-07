4ac013c73a9346b611ddc2bfddee49b358e08240
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-02-07 17:02:50 +0000

    Comment SingleRGBDImagePipe.

--- modules/spaint/include/spaint/imagesources/SingleRGBDImagePipe.h
+++ modules/spaint/include/spaint/imagesources/SingleRGBDImagePipe.h
@@ -14,31 +14,34 @@
 namespace spaint {
 
 /**
- * \brief TODO
+ * \brief An instance of this class represents a pipe to which individual RGB-D images can be
+ *        written so as to feed them to a SLAM component.
  */
 class SingleRGBDImagePipe : public InputSource::ImageSourceEngine
 {
   //#################### PRIVATE VARIABLES ####################
 private:
-  /** TODO */
+  /** The intrinsic calibration parameters for the camera producing the RGB-D images being fed through the pipe. */
   ITMLib::ITMRGBDCalib m_calib;
 
-  /** TODO */
+  /** The current depth image being fed through the pipe. */
   ITMShortImage_CPtr m_depthImage;
 
-  /** TODO */
+  /** The size of depth image being fed through the pipe. */
   Vector2i m_depthImageSize;
 
-  /** TODO */
+  /** The current RGB image being fed through the pipe. */
   ITMUChar4Image_CPtr m_rgbImage;
 
-  /** TODO */
+  /** The size of RGB image being fed through the pipe. */
   Vector2i m_rgbImageSize;
 
   //#################### CONSTRUCTORS ####################
 public:
   /**
-   * \brief TODO
+   * \brief Constructs a pipe to which individual RGB-D images can be written so as to feed them to a SLAM component.
+   *
+   * \param imageSourceEngine The engine that will be providing the RGB-D images that will be fed through the pipe.
    */
   explicit SingleRGBDImagePipe(const ImageSourceEngine_CPtr& imageSourceEngine);
 
@@ -60,9 +63,12 @@ public:
   virtual bool hasMoreImages() const;
 
   /**
-   * \brief TODO
+   * \brief Sets the current RGB and depth images to feed through the pipe.
+   *
+   * \param rgbImage    The current RGB image to feed through the pipe.
+   * \param depthImage  The current depth image to feed through the pipe.
    */
-  void set_images(const ITMUChar4Image_CPtr& rgbImage, const ITMShortImage_CPtr& rawDepthImage);
+  void set_images(const ITMUChar4Image_CPtr& rgbImage, const ITMShortImage_CPtr& depthImage);
 };
 
 //#################### TYPEDEFS ####################
