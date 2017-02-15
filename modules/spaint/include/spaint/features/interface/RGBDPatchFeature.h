2fa1152f2193cfb3245ea933054f6b5e04b2baae
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-02-15 17:07:18 +0000

    Keep tidying up.

--- modules/spaint/include/spaint/features/interface/RGBDPatchFeature.h
+++ modules/spaint/include/spaint/features/interface/RGBDPatchFeature.h
@@ -9,25 +9,31 @@
 #include <boost/shared_ptr.hpp>
 
 #include <ITMLib/Utils/ITMMath.h>
+
 #include <ORUtils/Image.h>
 
 namespace spaint
 {
 
 /**
- * \brief An instance of this type represents a 3D Keypoint with an associated colour.
+ * \brief An instance of this struct represents a 3D keypoint with an associated colour.
  */
 struct Keypoint3DColour
 {
-  /** The keypoint position in space. */
-  Vector3f position;
-  /** The keypoint colour. */
+  //#################### PUBLIC VARIABLES ####################
+
+  /** The keypoint's colour. */
   Vector3u colour;
-  /** A flag representing the validity of the Keypoint. */
+
+  /** The keypoint's position in space. */
+  Vector3f position;
+
+  /** A flag representing the validity of the keypoint. */
   bool valid;
 };
 
-/** Typedefs. */
+//#################### TYPEDEFS ####################
+
 typedef ORUtils::Image<Keypoint3DColour> Keypoint3DColourImage;
 typedef boost::shared_ptr<Keypoint3DColourImage> Keypoint3DColourImage_Ptr;
 typedef boost::shared_ptr<const Keypoint3DColourImage> Keypoint3DColourImage_CPtr;
@@ -41,29 +47,36 @@ typedef boost::shared_ptr<const Keypoint3DColourImage> Keypoint3DColourImage_CPt
  */
 struct RGBDPatchDescriptor
 {
+  //#################### CONSTANTS ####################
+
   /** Offsets to address the internal union-ed arrays. */
   static const int DEPTH_OFFSET = 0;
   static const int DEPTH_FEATURE_COUNT = 128;
   static const int RGB_OFFSET = DEPTH_FEATURE_COUNT;
   static const int RGB_FEATURE_COUNT = 128;
-  static const int FEATURE_SIZE = RGB_FEATURE_COUNT + DEPTH_FEATURE_COUNT;
+  static const int FEATURE_COUNT = RGB_FEATURE_COUNT + DEPTH_FEATURE_COUNT;
 
-  /** Anonymous union used to access different parts of the descriptor. */
+  //#################### PUBLIC VARIABLES ####################
+
+  /** An anonymous union used to access different parts of the descriptor. */
   union
   {
-    /** All the values are stored in this array. */
-    float data[FEATURE_SIZE];
+    /** All the features are stored in this array. */
+    float data[FEATURE_COUNT];
+
     struct
     {
-      /** Gives access to the depth-based values of the descriptor. */
+      /** Gives access to the depth-based features in the descriptor. */
       float depth[DEPTH_FEATURE_COUNT];
-      /** Gives access to the colour-based values of the descriptor. */
+
+      /** Gives access to the colour-based features in the descriptor. */
       float rgb[RGB_FEATURE_COUNT];
     };
   };
 };
 
-/** Typedefs. */
+//#################### TYPEDEFS ####################
+
 typedef ORUtils::Image<RGBDPatchDescriptor> RGBDPatchDescriptorImage;
 typedef boost::shared_ptr<RGBDPatchDescriptorImage> RGBDPatchDescriptorImage_Ptr;
 typedef boost::shared_ptr<const RGBDPatchDescriptorImage> RGBDPatchDescriptorImage_CPtr;
