166f6694b843db0aec657ea01e04e710a676a6c8
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-02-17 18:12:18 +0000

    Continue moving the explicit instantiations up to the top level.

--- modules/grove/src/CPUInstantiations.cpp
+++ modules/grove/src/CPUInstantiations.cpp
@@ -3,6 +3,7 @@
  * Copyright (c) Torr Vision Group, University of Oxford, 2017. All rights reserved.
  */
 
+#include "features/cpu/RGBDPatchFeatureCalculator_CPU.tpp"
 #include "features/interface/RGBDPatchFeatureCalculator.tpp"
 
 namespace grove {
@@ -11,5 +12,7 @@ namespace grove {
 
 template class RGBDPatchFeatureCalculator<Keypoint2D, RGBDPatchDescriptor>;
 template class RGBDPatchFeatureCalculator<Keypoint3DColour, RGBDPatchDescriptor>;
+template class RGBDPatchFeatureCalculator_CPU<Keypoint2D, RGBDPatchDescriptor>;
+template class RGBDPatchFeatureCalculator_CPU<Keypoint3DColour, RGBDPatchDescriptor>;
 
 }
