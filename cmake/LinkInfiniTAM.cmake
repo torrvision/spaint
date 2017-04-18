c77b2e89ba2267bf53c53d0aacaa8be112a55455
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-04-18 15:27:10 +0100

    Get rid of the WITH_INFINITAM option.

--- cmake/LinkInfiniTAM.cmake
+++ cmake/LinkInfiniTAM.cmake
@@ -2,10 +2,8 @@
 # LinkInfiniTAM.cmake #
 #######################
 
-IF(WITH_INFINITAM)
-  TARGET_LINK_LIBRARIES(${targetname} ${InfiniTAM_LIBRARIES})
+TARGET_LINK_LIBRARIES(${targetname} ${InfiniTAM_LIBRARIES})
 
-  IF(PNG_FOUND)
-    TARGET_LINK_LIBRARIES(${targetname} ${PNG_LIBRARIES})
-  ENDIF()
+IF(PNG_FOUND)
+  TARGET_LINK_LIBRARIES(${targetname} ${PNG_LIBRARIES})
 ENDIF()
