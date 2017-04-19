f5ea96ca6268f4631271f80afe375ee0a76cd25f
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2017-04-19 11:41:01 +0100

    build on linux when WITH_SCOREFORESTS is ON
    also, grove needs to link with itmx

--- cmake/LinkGrove.cmake
+++ cmake/LinkGrove.cmake
@@ -3,7 +3,7 @@
 ###################
 
 IF(BUILD_GROVE)
-  TARGET_LINK_LIBRARIES(${targetname} grove tvgutil ${CUDA_cudadevrt_LIBRARY})
+  TARGET_LINK_LIBRARIES(${targetname} grove tvgutil itmx ${CUDA_cudadevrt_LIBRARY})
 
   INCLUDE(${PROJECT_SOURCE_DIR}/cmake/LinkScoreForests.cmake)
 ENDIF()
