a2f5bba2ccdc2c2fca9de980d8b6f2ef9da48148
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-04-20 15:47:04 +0100

    Undo the changes to the tvgutil scratch test.

--- tests/scratch/tvgutil/main.cpp
+++ tests/scratch/tvgutil/main.cpp
@@ -1,67 +1,6 @@
 //###
 #if 1
 
-#include <algorithm>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <stdexcept>
-#include <string>
-#include <vector>
-
-#include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
-
-#include <tvgutil/containers/MapUtil.h>
-using namespace tvgutil;
-
-class AppParams
-{
-private:
-  typedef std::map<std::string,boost::any> Base;
-  Base m_base;
-public:
-  template <typename V>
-  void insert(const std::string& key, const V& value)
-  {
-    m_base.insert(std::make_pair(key, value));
-  }
-
-  void insert(const std::string& key, const char *value)
-  {
-    m_base.insert(std::make_pair(key, std::string(value)));
-  }
-
-  template <typename V>
-  V operator()(const std::string& key, const V& /* dummy */) const
-  {
-    Base::const_iterator it = m_base.find(key);
-    if(it != m_base.end()) return boost::any_cast<V>(it->second);
-    else throw std::runtime_error("...");
-  }
-};
-
-int main()
-{
-  AppParams ps;
-  ps.insert("Foo", 23);
-  ps.insert("Bar", "Wibble");
-  std::vector<std::string> vec;
-  vec.push_back("9");
-  ps.insert("Baz", vec);
-  int i = ps("Foo", i);
-  std::string s = ps("Bar", s);
-  std::vector<std::string> v = ps("Baz", v);
-  std::cout << i << ' ' << s << '\n';
-  std::copy(v.begin(), v.end(), std::ostream_iterator<std::string>(std::cout, " "));
-  return 0;
-}
-
-#endif
-
-//###
-#if 0
-
 #include <iostream>
 
 #include <boost/thread.hpp>
