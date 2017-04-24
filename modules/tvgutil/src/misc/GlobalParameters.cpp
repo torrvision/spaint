a37c7b5d399acd3c197cc429dbd2b73d43588081
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-04-24 12:04:54 +0100

    Minor tidying changes.

--- modules/tvgutil/src/misc/GlobalParameters.cpp
+++ modules/tvgutil/src/misc/GlobalParameters.cpp
@@ -1,6 +1,6 @@
 /**
  * tvgutil: GlobalParameters.cpp
- * Copyright (c) Torr Vision Group, University of Oxford, 2015. All rights reserved.
+ * Copyright (c) Torr Vision Group, University of Oxford, 2017. All rights reserved.
  */
 
 #include "misc/GlobalParameters.h"
@@ -19,23 +19,24 @@ GlobalParameters& GlobalParameters::instance()
 
 //#################### PUBLIC MEMBER FUNCTIONS ####################
 
-void GlobalParameters::add_value(const std::string &key, const std::string &value)
+void GlobalParameters::add_value(const std::string& key, const std::string& value)
 {
   m_params[key].push_back(value);
 }
 
 //#################### STREAM OPERATORS ####################
 
-std::ostream& operator<<(std::ostream &os, const GlobalParameters& rhs)
+std::ostream& operator<<(std::ostream& os, const GlobalParameters& rhs)
 {
-  for(std::map<std::string, std::vector<std::string> >::const_iterator it = rhs.m_params.begin(); it != rhs.m_params.end(); ++it)
+  for(std::map<std::string,std::vector<std::string> >::const_iterator it = rhs.m_params.begin(), iend = rhs.m_params.end(); it != iend; ++it)
   {
     os << it->first << ":\n";
-    for(std::vector<std::string>::const_iterator valueIt = it->second.begin(); valueIt != it->second.end(); ++valueIt)
+    for(std::vector<std::string>::const_iterator jt = it->second.begin(), jend = it->second.end(); jt != jend; ++jt)
     {
-      os << '\t' << *valueIt << '\n';
+      os << '\t' << *jt << '\n';
     }
   }
+
   return os;
 }
 
