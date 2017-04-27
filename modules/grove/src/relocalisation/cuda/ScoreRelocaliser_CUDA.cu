03a87d84cf58e98229ad5c24ee324dbebd21ddd6
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2017-04-27 14:25:34 +0100

    Tidy up the cuda relocaliser class

--- modules/grove/src/relocalisation/cuda/ScoreRelocaliser_CUDA.cu
+++ modules/grove/src/relocalisation/cuda/ScoreRelocaliser_CUDA.cu
@@ -23,66 +23,79 @@ using itmx::MemoryBlockFactory;
 
 namespace grove {
 
-template<int TREE_COUNT>
-__global__ void ck_get_predictions(
-    const ScorePrediction* leafPredictions,
-    const ORUtils::VectorX<int, TREE_COUNT>* leafIndices,
-    ScorePrediction* outPredictions,
-    Vector2i imgSize)
+//#################### CUDA KERNELS ####################
+
+template <int TREE_COUNT>
+__global__ void ck_score_relocaliser_get_predictions(const ScorePrediction *leafPredictions,
+                                                     const ORUtils::VectorX<int, TREE_COUNT> *leafIndices,
+                                                     ScorePrediction *outPredictions,
+                                                     Vector2i imgSize)
 {
   const int x = blockIdx.x * blockDim.x + threadIdx.x;
   const int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-  if (x >= imgSize.x || y >= imgSize.y)
-    return;
+  if (x >= imgSize.x || y >= imgSize.y) return;
 
   get_prediction_for_leaf_shared(leafPredictions, leafIndices, outPredictions, imgSize, x, y);
 }
 
-ScoreRelocaliser_CUDA::ScoreRelocaliser_CUDA(const std::string &forestFilename)
-  : ScoreRelocaliser(forestFilename)
+//#################### CONSTRUCTORS ####################
+
+ScoreRelocaliser_CUDA::ScoreRelocaliser_CUDA(const std::string &forestFilename) : ScoreRelocaliser(forestFilename)
 {
+  // Instantiate the sub-algorithms knowing that we are on the GPU.
+
+  // Features.
   m_featureCalculator = FeatureCalculatorFactory::make_da_rgbd_patch_feature_calculator(ITMLibSettings::DEVICE_CUDA);
 
-  m_scoreForest = DecisionForestFactory<DescriptorType, FOREST_TREE_COUNT>::make_forest(ITMLibSettings::DEVICE_CUDA, m_forestFilename);
+  // LowLevelEngine.
+  m_lowLevelEngine.reset(ITMLowLevelEngineFactory::MakeLowLevelEngine(ITMLibSettings::DEVICE_CUDA));
 
-  // These variables have to be set here, since they depend on the forest.
+  // Forest.
+  m_scoreForest = DecisionForestFactory<DescriptorType, FOREST_TREE_COUNT>::make_forest(ITMLibSettings::DEVICE_CUDA,
+                                                                                        m_forestFilename);
+
+  // These variables have to be set here, since they depend on the forest that has just been loaded.
   m_reservoirsCount = m_scoreForest->get_nb_leaves();
   m_predictionsBlock = MemoryBlockFactory::instance().make_block<ScorePrediction>(m_reservoirsCount);
 
-  m_exampleReservoirs = ExampleReservoirsFactory<ExampleType>::make_reservoirs(ITMLibSettings::DEVICE_CUDA, m_reservoirsCapacity, m_reservoirsCount, m_rngSeed);
-  m_exampleClusterer = ExampleClustererFactory<ExampleType, ClusterType>::make_clusterer(ITMLibSettings::DEVICE_CUDA, m_clustererSigma, m_clustererTau, m_maxClusterCount, m_minClusterSize);
+  // Reservoirs.
+  m_exampleReservoirs = ExampleReservoirsFactory<ExampleType>::make_reservoirs(
+      ITMLibSettings::DEVICE_CUDA, m_reservoirsCapacity, m_reservoirsCount, m_rngSeed);
 
-  // RANSAC is forced to the CPU implementation for now, the CUDA one is slower.
-//  m_preemptiveRansac = RansacFactory::make_preemptive_ransac(ITMLibSettings::DEVICE_CPU);
+  // Clustering.
+  m_exampleClusterer = ExampleClustererFactory<ExampleType, ClusterType>::make_clusterer(
+      ITMLibSettings::DEVICE_CUDA, m_clustererSigma, m_clustererTau, m_maxClusterCount, m_minClusterSize);
+
+  // P-RANSAC.
   m_preemptiveRansac = RansacFactory::make_preemptive_ransac(ITMLibSettings::DEVICE_CUDA);
 
-  m_lowLevelEngine.reset(ITMLowLevelEngineFactory::MakeLowLevelEngine(ITMLibSettings::DEVICE_CUDA));
-
-  // Clear state
+  // Clear internal state.
   reset();
 }
 
-void ScoreRelocaliser_CUDA::get_predictions_for_leaves(
-    const LeafIndicesImage_CPtr &leafIndices,
-    const ScorePredictionsBlock_CPtr &leafPredictions,
-    ScorePredictionsImage_Ptr &outputPredictions) const
+//#################### PROTECTED VIRTUAL MEMBER FUNCTIONS ####################
+
+void ScoreRelocaliser_CUDA::get_predictions_for_leaves(const LeafIndicesImage_CPtr &leafIndices,
+                                                       const ScorePredictionsBlock_CPtr &leafPredictions,
+                                                       ScorePredictionsImage_Ptr &outputPredictions) const
 {
   const Vector2i imgSize = leafIndices->noDims;
-  const LeafIndices* leafIndicesData = leafIndices->GetData(MEMORYDEVICE_CUDA);
+  const LeafIndices *leafIndicesData = leafIndices->GetData(MEMORYDEVICE_CUDA);
 
   // Leaf predictions
   const ScorePrediction *leafPredictionsData = leafPredictions->GetData(MEMORYDEVICE_CUDA);
 
-  // No-op after the first time.
+  // NOP after the first time.
   outputPredictions->ChangeDims(imgSize);
   ScorePrediction *outPredictionsData = outputPredictions->GetData(MEMORYDEVICE_CUDA);
 
   const dim3 blockSize(32, 32);
   const dim3 gridSize((imgSize.x + blockSize.x - 1) / blockSize.x, (imgSize.y + blockSize.y - 1) / blockSize.y);
 
-  ck_get_predictions<<<gridSize,blockSize>>>(leafPredictionsData, leafIndicesData, outPredictionsData, imgSize);
+  ck_score_relocaliser_get_predictions<<<gridSize, blockSize>>>(
+      leafPredictionsData, leafIndicesData, outPredictionsData, imgSize);
   ORcudaKernelCheck;
 }
 
-}
+} // namespace grove
