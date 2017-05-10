d797063d77466ac74fe4dd24ce8293eac78991e2
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-05-10 16:15:32 +0100

    Get rid of TrackerType and import the ITMObjectPtrTypes changes.

--- modules/itmx/include/itmx/ITMObjectPtrTypes.h
+++ modules/itmx/include/itmx/ITMObjectPtrTypes.h
@@ -6,7 +6,9 @@
 #include <boost/shared_ptr.hpp>
 
 #include <InputSource/CompositeImageSourceEngine.h>
+#include <ITMLib/Core/ITMTrackingController.h>
 #include <ITMLib/Engines/LowLevel/Interface/ITMLowLevelEngine.h>
+#include <ITMLib/Engines/ViewBuilding/Interface/ITMViewBuilder.h>
 #include <ITMLib/Objects/Misc/ITMIMUCalibrator.h>
 #include <ITMLib/Objects/RenderStates/ITMRenderState.h>
 #include <ITMLib/Objects/RenderStates/ITMSurfelRenderState.h>
@@ -22,8 +24,10 @@ typedef boost::shared_ptr<ITMLib::ITMLowLevelEngine> LowLevelEngine_Ptr;
 typedef boost::shared_ptr<ITMLib::ITMLibSettings> Settings_Ptr;
 typedef boost::shared_ptr<ITMLib::ITMSurfelRenderState> SurfelRenderState_Ptr;
 typedef boost::shared_ptr<ITMLib::ITMTracker> Tracker_Ptr;
+typedef boost::shared_ptr<ITMLib::ITMTrackingController> TrackingController_Ptr;
 typedef boost::shared_ptr<ITMLib::ITMTrackingState> TrackingState_Ptr;
 typedef boost::shared_ptr<ITMLib::ITMView> View_Ptr;
+typedef boost::shared_ptr<ITMLib::ITMViewBuilder> ViewBuilder_Ptr;
 typedef boost::shared_ptr<ITMLib::ITMRenderState> VoxelRenderState_Ptr;
 
 typedef boost::shared_ptr<const InputSource::CompositeImageSourceEngine> CompositeImageSourceEngine_CPtr;
@@ -33,6 +37,8 @@ typedef boost::shared_ptr<const ITMLib::ITMLowLevelEngine> LowLevelEngine_CPtr;
 typedef boost::shared_ptr<const ITMLib::ITMLibSettings> Settings_CPtr;
 typedef boost::shared_ptr<const ITMLib::ITMSurfelRenderState> SurfelRenderState_CPtr;
 typedef boost::shared_ptr<const ITMLib::ITMTracker> Tracker_CPtr;
+typedef boost::shared_ptr<const ITMLib::ITMTrackingController> TrackingController_CPtr;
 typedef boost::shared_ptr<const ITMLib::ITMTrackingState> TrackingState_CPtr;
 typedef boost::shared_ptr<const ITMLib::ITMView> View_CPtr;
+typedef boost::shared_ptr<const ITMLib::ITMViewBuilder> ViewBuilder_CPtr;
 typedef boost::shared_ptr<const ITMLib::ITMRenderState> VoxelRenderState_CPtr;
