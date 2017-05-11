3ba18dba7faae44f3f8e77790443ca350b371657
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-05-11 14:06:51 +0100

    Minor updates to PosePersister.

--- modules/itmx/src/PosePersister.cpp
+++ modules/itmx/src/PosePersister.cpp
@@ -1,5 +1,5 @@
 /**
- * itmx: ImagePersister.cpp
+ * itmx: PosePersister.cpp
  * Copyright (c) Torr Vision Group, University of Oxford, 2016. All rights reserved.
  */
 
@@ -8,9 +8,10 @@
 #include <fstream>
 #include <stdexcept>
 
-#include <ITMLib/Utils/ITMMath.h>
-
 #include <tvgutil/misc/ThreadPool.h>
+using tvgutil::ThreadPool;
+
+namespace bf = boost::filesystem;
 
 namespace itmx {
 
@@ -18,35 +19,33 @@ namespace itmx {
 
 void PosePersister::save_pose(const Matrix4f& pose, const std::string& path)
 {
-  // Open the output file.
-  std::ofstream outFile(path.c_str());
+  // Attempt to open the output file.
+  std::ofstream fs(path.c_str());
+  if(!fs) throw std::runtime_error("Could not open output file: " + path);
 
-  // If the open failed throw an exception.
-  if (!outFile) throw std::runtime_error("Could not open output file: " + path);
-
-  // Print the matrix on the file
-  // Not using Matrix4f::operator<< because it adds commas between matrix entries.
+  // Write the matrix to the file. Note that we avoid using the matrix-level operator<<
+  // because it adds commas between the matrix entries.
   for (int y = 0; y < 4; ++y)
   {
-    outFile << pose(0, y) << ' ' << pose(1, y) << ' ' << pose(2, y) << ' ' << pose(3, y) << '\n';
+    fs << pose(0, y) << ' ' << pose(1, y) << ' ' << pose(2, y) << ' ' << pose(3, y) << '\n';
   }
 }
 
-void PosePersister::save_pose(const Matrix4f& pose, const boost::filesystem::path& path)
+void PosePersister::save_pose(const Matrix4f& pose, const bf::path& path)
 {
   save_pose(pose, path.string());
 }
 
 void PosePersister::save_pose_on_thread(const Matrix4f& pose, const std::string& path)
 {
-  // Select the overload taking a string.
-  void (*s) (const Matrix4f&, const std::string&) = &save_pose;
+  // Select the save_pose overload that takes a string.
+  void (*f)(const Matrix4f&, const std::string&) = &save_pose;
 
-  // Call save_pose on a separate thread
-  tvgutil::ThreadPool::instance().post_task(boost::bind(s, pose, path));
+  // Call it on a separate thread.
+  ThreadPool::instance().post_task(boost::bind(f, pose, path));
 }
 
-void PosePersister::save_pose_on_thread(const Matrix4f& pose, const boost::filesystem::path& path)
+void PosePersister::save_pose_on_thread(const Matrix4f& pose, const bf::path& path)
 {
   save_pose_on_thread(pose, path.string());
 }
