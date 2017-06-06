7772085e055b7a4a660ec9909209c9826c3fdba3
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-06-06 16:26:19 +0100

    Tidy up SettingsContainer.

--- modules/tvgutil/src/misc/SettingsContainer.cpp
+++ modules/tvgutil/src/misc/SettingsContainer.cpp
@@ -7,18 +7,22 @@
 
 namespace tvgutil {
 
+//#################### DESTRUCTOR ####################
+
+SettingsContainer::~SettingsContainer() {}
+
 //#################### PUBLIC MEMBER FUNCTIONS ####################
 
 void SettingsContainer::add_value(const std::string& key, const std::string& value)
 {
-  m_params[key].push_back(value);
+  m_settings[key].push_back(value);
 }
 
 //#################### STREAM OPERATORS ####################
 
 std::ostream& operator<<(std::ostream& os, const SettingsContainer& rhs)
 {
-  for(std::map<std::string,std::vector<std::string> >::const_iterator it = rhs.m_params.begin(), iend = rhs.m_params.end(); it != iend; ++it)
+  for(std::map<std::string,std::vector<std::string> >::const_iterator it = rhs.m_settings.begin(), iend = rhs.m_settings.end(); it != iend; ++it)
   {
     os << it->first << ":\n";
     for(std::vector<std::string>::const_iterator jt = it->second.begin(), jend = it->second.end(); jt != jend; ++jt)
