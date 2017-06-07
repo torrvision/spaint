4c9d2381c6e53f66faf3ebf6809e0cc163efa6f8
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2017-06-07 15:32:26 +0100

    update path

--- apps/spaintgui/resources/scripts/spaint_batch.sh
+++ apps/spaintgui/resources/scripts/spaint_batch.sh
@@ -15,6 +15,6 @@ pose_mask='frame-%06d.pose.txt'
 
 for seq in $sequences; do
 #  CUDA_VISIBLE_DEVICES=1 time ../../spaintgui -c "$dataset_root/calib.txt" -d "$dataset_root/$seq/train/$depth_mask" -r "$dataset_root/$seq/train/$color_mask" -p "$dataset_root/$seq/train/$pose_mask" -t Disk -d "$dataset_root/$seq/test/$depth_mask" -r "$dataset_root/$seq/test/$color_mask" -t ForceFail --pipelineType slam --batch --experimentTag "$tag"_"$seq" -f "7scenes.ini" # --saveMeshOnExit
-  CUDA_VISIBLE_DEVICES=0 time ./bin/apps/spaintgui/spaintgui -c "$dataset_root/calib.txt" -d "$dataset_root/$seq/train/$depth_mask" -r "$dataset_root/$seq/train/$color_mask" -p "$dataset_root/$seq/train/$pose_mask" -t Disk -d "$dataset_root/$seq/test/$depth_mask" -r "$dataset_root/$seq/test/$color_mask" -t ForceFail --pipelineType slam --batch --experimentTag "$tag"_"$seq" -f "7scenes.ini" # --saveMeshOnExit
+  CUDA_VISIBLE_DEVICES=0 time ../../spaintgui -c "$dataset_root/calib.txt" -d "$dataset_root/$seq/train/$depth_mask" -r "$dataset_root/$seq/train/$color_mask" -p "$dataset_root/$seq/train/$pose_mask" -t Disk -d "$dataset_root/$seq/test/$depth_mask" -r "$dataset_root/$seq/test/$color_mask" -t ForceFail --pipelineType slam --batch --experimentTag "$tag"_"$seq" -f "7scenes.ini" # --saveMeshOnExit
 done
 
