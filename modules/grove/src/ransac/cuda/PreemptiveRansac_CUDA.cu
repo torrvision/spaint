67d13739cbc08361934cb69095cfaae94fd0d602
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-06-16 14:15:13 +0100

    Suppress some Thrust warnings.

--- modules/grove/src/ransac/cuda/PreemptiveRansac_CUDA.cu
+++ modules/grove/src/ransac/cuda/PreemptiveRansac_CUDA.cu
@@ -7,7 +7,7 @@
 
 #ifdef _MSC_VER
   // Suppress some VC++ warnings that are produced when including the Thrust headers.
-  #pragma warning(disable:4267)
+  #pragma warning(disable:4244 4267)
 #endif
 
 #include <thrust/device_ptr.h>
@@ -15,7 +15,7 @@
 
 #ifdef _MSC_VER
   // Reenable the suppressed warnings for the rest of the translation unit.
-  #pragma warning(default:4267)
+  #pragma warning(default:4244 4267)
 #endif
 
 #include <itmx/base/MemoryBlockFactory.h>
