4eacfb9579c25423107b1c0fb55118964afdfa08
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-09-19 16:43:33 +0100

    Start working on background relocalisation.

---
+++ modules/itmx/include/itmx/relocalisation/BackgroundRelocaliser.h
@@ -0,0 +1,79 @@
+/**
+ * itmx: BackgroundRelocaliser.h
+ * Copyright (c) Torr Vision Group, University of Oxford, 2017. All rights reserved.
+ */
+
+#ifndef H_ITMX_BACKGROUNDRELOCALISER
+#define H_ITMX_BACKGROUNDRELOCALISER
+
+#include "Relocaliser.h"
+#include "../base/ITMImagePtrTypes.h"
+
+namespace itmx {
+
+/**
+ * \brief An instance of this class can be used to decorate calls to a relocaliser
+ *        so that they are performed in the background on a different GPU.
+ */
+class BackgroundRelocaliser : public Relocaliser
+{
+  //#################### PRIVATE VARIABLES ####################
+private:
+  /** TODO */
+  mutable ITMUChar4Image_Ptr m_colourImage;
+
+  /** TODO */
+  mutable ITMFloatImage_Ptr m_depthImage;
+
+  /** The ID of the old GPU on which calls were previously being performed, so that it can be restored later. */
+  mutable int m_oldDevice;
+
+  /** The ID of the GPU on which relocalisation calls should be performed. */
+  int m_relocalisationDevice;
+
+  /** The relocaliser to decorate. */
+  Relocaliser_Ptr m_relocaliser;
+
+  //#################### CONSTRUCTORS ####################
+public:
+  /**
+   * \brief TODO
+   */
+  BackgroundRelocaliser(const Relocaliser_Ptr& relocaliser, int relocalisationDevice);
+
+  //#################### PUBLIC MEMBER FUNCTIONS ####################
+public:
+  /** Override */
+  virtual boost::optional<Result> relocalise(const ITMUChar4Image *colourImage, const ITMFloatImage *depthImage, const Vector4f& depthIntrinsics) const;
+
+  /** Override */
+  virtual void reset();
+
+  /** Override */
+  virtual void train(const ITMUChar4Image *colourImage, const ITMFloatImage *depthImage,
+                     const Vector4f& depthIntrinsics, const ORUtils::SE3Pose& cameraPose);
+
+  /** Override */
+  virtual void update();
+
+  //#################### PRIVATE MEMBER FUNCTIONS ####################
+private:
+  /**
+   * \brief TODO
+   */
+  void copy_images(const ITMUChar4Image *colourImage, const ITMFloatImage *depthImage) const;
+
+  /**
+   * \brief TODO
+   */
+  void to_old_gpu() const;
+
+  /**
+   * \brief TODO
+   */
+  void to_relocalisation_gpu() const;
+};
+
+}
+
+#endif
---
+++ modules/itmx/src/relocalisation/BackgroundRelocaliser.cpp
@@ -0,0 +1,82 @@
+/**
+ * itmx: BackgroundRelocaliser.cpp
+ * Copyright (c) Torr Vision Group, University of Oxford, 2017. All rights reserved.
+ */
+
+#include "relocalisation/BackgroundRelocaliser.h"
+
+namespace itmx {
+
+//#################### CONSTRUCTORS ####################
+
+BackgroundRelocaliser::BackgroundRelocaliser(const Relocaliser_Ptr& relocaliser, int relocalisationDevice)
+: m_relocalisationDevice(relocalisationDevice), m_relocaliser(relocaliser)
+{}
+
+//#################### PUBLIC MEMBER FUNCTIONS ####################
+
+boost::optional<Relocaliser::Result> BackgroundRelocaliser::relocalise(const ITMUChar4Image *colourImage, const ITMFloatImage *depthImage, const Vector4f& depthIntrinsics) const
+{
+  colourImage->UpdateHostFromDevice();
+  depthImage->UpdateHostFromDevice();
+  to_relocalisation_gpu();
+  copy_images(colourImage, depthImage);
+  boost::optional<Relocaliser::Result> result = m_relocaliser->relocalise(m_colourImage.get(), m_depthImage.get(), depthIntrinsics);
+  to_old_gpu();
+  return result;
+}
+
+void BackgroundRelocaliser::reset()
+{
+  to_relocalisation_gpu();
+  m_relocaliser->reset();
+  to_old_gpu();
+}
+
+void BackgroundRelocaliser::train(const ITMUChar4Image *colourImage, const ITMFloatImage *depthImage,
+                                  const Vector4f& depthIntrinsics, const ORUtils::SE3Pose& cameraPose)
+{
+  colourImage->UpdateHostFromDevice();
+  depthImage->UpdateHostFromDevice();
+  to_relocalisation_gpu();
+  copy_images(colourImage, depthImage);
+  m_relocaliser->train(m_colourImage.get(), m_depthImage.get(), depthIntrinsics, cameraPose);
+  to_old_gpu();
+}
+
+void BackgroundRelocaliser::update()
+{
+  to_relocalisation_gpu();
+  m_relocaliser->update();
+  to_old_gpu();
+}
+
+//#################### PRIVATE MEMBER FUNCTIONS ####################
+
+void BackgroundRelocaliser::copy_images(const ITMUChar4Image *colourImage, const ITMFloatImage *depthImage) const
+{
+  if(!m_colourImage) m_colourImage.reset(new ITMUChar4Image(colourImage->noDims, true, true));
+  if(!m_depthImage) m_depthImage.reset(new ITMFloatImage(depthImage->noDims, true, true));
+
+  m_colourImage->ChangeDims(colourImage->noDims);
+  m_depthImage->ChangeDims(depthImage->noDims);
+
+  m_colourImage->SetFrom(colourImage, ITMUChar4Image::CPU_TO_CPU);
+  m_depthImage->SetFrom(depthImage, ITMFloatImage::CPU_TO_CPU);
+
+  m_colourImage->UpdateDeviceFromHost();
+  m_depthImage->UpdateDeviceFromHost();
+}
+
+void BackgroundRelocaliser::to_old_gpu() const
+{
+  cudaSetDevice(m_oldDevice);
+}
+
+void BackgroundRelocaliser::to_relocalisation_gpu() const
+{
+  cudaGetDevice(&m_oldDevice);
+  cudaSetDevice(m_relocalisationDevice);
+}
+
+}
