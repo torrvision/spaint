ba17a9ba09d24ffef3bdda4abe6177bfbdf25273
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-09-23 18:04:21 +0100

    Fix a comment.

--- modules/tvgutil/include/tvgutil/filesystem/SequentialPathGenerator.h
+++ modules/tvgutil/include/tvgutil/filesystem/SequentialPathGenerator.h
@@ -44,9 +44,9 @@ public:
   const boost::filesystem::path& get_base_dir() const;
 
   /**
-   * \brief Gets the current value of the index in the sequence.
+   * \brief Gets the current index in the sequence.
    *
-   * \return The value of the index in the sequence.
+   * \return The current index in the sequence.
    */
   int get_index() const;
 
