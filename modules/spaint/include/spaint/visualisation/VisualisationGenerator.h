4ba0295a5c3da12a748d802ea5f823386abf0140
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-10-13 16:31:18 +0100

    Comment generate_depth_from_voxels.

--- modules/spaint/include/spaint/visualisation/VisualisationGenerator.h
+++ modules/spaint/include/spaint/visualisation/VisualisationGenerator.h
@@ -89,7 +89,17 @@ public:
   //#################### PUBLIC MEMBER FUNCTIONS ####################
 public:
   /**
-   * \brief TODO
+   * \brief Generates a synthetic depth image of a voxel scene from the specified pose.
+   *
+   * \note  This produces a floating-point depth image whose format matches that used by InfiniTAM,
+   *        as opposed to a colourised depth image that is suitable for showing to the user.
+   *
+   * \param output      The location into which to put the output image.
+   * \param scene       The scene to visualise.
+   * \param pose        The pose from which to visualise the scene.
+   * \param view        The current view of the scene (used only for the camera settings).
+   * \param renderState The render state to use for intermediate storage.
+   * \param depthType   The type of depth calculation to use.
    */
   void generate_depth_from_voxels(const ITMFloatImage_Ptr& output, const SpaintVoxelScene_CPtr& scene, const ORUtils::SE3Pose& pose,
                                   const View_CPtr& view, VoxelRenderState_Ptr& renderState, DepthVisualiser::DepthType depthType) const;
