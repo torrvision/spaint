ab8c5a01da76f29906730acb27ccec3db2800546
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-11-05 00:32:01 +0000

    Revert a change.

--- modules/spaint/src/pipelinecomponents/CollaborativeComponent.cpp
+++ modules/spaint/src/pipelinecomponents/CollaborativeComponent.cpp
@@ -371,7 +371,7 @@ bool CollaborativeComponent::update_trajectories()
     TrackingState_CPtr trackingState = slamState->get_tracking_state();
     const ITMUChar4Image *rgbImage = slamState->get_view()->rgb;
     rgbImage->UpdateHostFromDevice();
-    if(inputStatus == SLAMState::IS_ACTIVE && trackingState->trackerResult == ITMTrackingState::TRACKING_GOOD/* && count_orb_keypoints(rgbImage) >= 400*/)
+    if(inputStatus == SLAMState::IS_ACTIVE && trackingState->trackerResult == ITMTrackingState::TRACKING_GOOD && count_orb_keypoints(rgbImage) >= 400)
     {
       m_trajectories[sceneIDs[i]].push_back(*trackingState->pose_d);
     }
