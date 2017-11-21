84aa3ab9acee90a0b93f98188a905fa7f5515d39
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-11-21 11:59:18 +0000

    Set things up for the video.

--- apps/spaintgui/renderers/Renderer.cpp
+++ apps/spaintgui/renderers/Renderer.cpp
@@ -491,7 +491,11 @@ void Renderer::render_reconstructed_scene(const std::string& sceneID, const SE3P
     depthImages.clear();
   }
 
+#if 0
   if(sceneID == "World")
+#else
+  if(&subwindow == &m_subwindowConfiguration->subwindow(0))
+#endif
   {
     while(images.size() < sceneIDs.size())
     {
@@ -511,7 +515,7 @@ void Renderer::render_reconstructed_scene(const std::string& sceneID, const SE3P
       {
         boost::optional<std::pair<SE3Pose,size_t> > result = m_model->get_collaborative_pose_optimiser()->try_get_relative_transform("World", sceneIDs[i]);
         SE3Pose relativeTransform = result ? result->first : SE3Pose(static_cast<float>((i + 1) * 2.0f), 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
-        if(!result || result->second < CollaborativePoseOptimiser::confidence_threshold()) visualisationTypes[i] = VisualisationGenerator::VT_SCENE_SEMANTICFLAT;
+        if(!result || result->second < CollaborativePoseOptimiser::confidence_threshold()) visualisationTypes[i] = VisualisationGenerator::VT_SCENE_SEMANTICLAMBERTIAN;
 
         // ciTwi * wiTwj = ciTwj
         tempPose.SetM(tempPose.GetM() * relativeTransform.GetM());
@@ -557,7 +561,11 @@ void Renderer::render_reconstructed_scene(const std::string& sceneID, const SE3P
 
 #if 1
   // FIXME: This is also a disgusting hack.
+#if 0
   if(sceneID == "World")
+#else
+  if(&subwindow == &m_subwindowConfiguration->subwindow(0))
+#endif
   {
     for(size_t k = 0; k < image->noDims.width * image->noDims.height; ++k)
     {
@@ -566,7 +574,7 @@ void Renderer::render_reconstructed_scene(const std::string& sceneID, const SE3P
       {
         const float arbitrarilyLargeDepth = 100.0f;
         float depth = depthImages[i]->GetData(MEMORYDEVICE_CPU)[k];
-        if(depth != -1.0f && visualisationTypes[i] == VisualisationGenerator::VT_SCENE_SEMANTICFLAT) depth = arbitrarilyLargeDepth;
+        if(depth != -1.0f && visualisationTypes[i] == VisualisationGenerator::VT_SCENE_SEMANTICLAMBERTIAN) depth = arbitrarilyLargeDepth;
         if(depth != -1.0f && depth < smallestDepth)
         {
           smallestDepth = depth;
