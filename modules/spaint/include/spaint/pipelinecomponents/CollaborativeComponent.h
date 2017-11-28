0d19fe660ab89e45837594adcd2588c95bb83856
Stuart Golodetz <sgolodetz@gxstudios.net>
2017-11-28 12:37:50 +0000

    Import Tommaso's fix for the visualisation glitch.

--- modules/spaint/include/spaint/pipelinecomponents/CollaborativeComponent.h
+++ modules/spaint/include/spaint/pipelinecomponents/CollaborativeComponent.h
@@ -16,6 +16,7 @@
 #include "CollaborativeContext.h"
 #include "../collaboration/CollaborationMode.h"
 #include "../collaboration/CollaborativeRelocalisation.h"
+#include "../visualisation/VisualisationGenerator.h"
 
 namespace spaint {
 
@@ -61,6 +62,9 @@ private:
 
   /** TODO */
   std::map<std::pair<std::string,std::string>,std::set<int> > m_triedFrameIndices;
+
+  /** A visualisation generator that is specific to this collaborative component. We avoid sharing one with other components for thread-safety reasons. */
+  VisualisationGenerator_CPtr m_visualisationGenerator;
 
   //#################### CONSTRUCTORS ####################
 public:
