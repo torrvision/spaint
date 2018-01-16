b880a603cdffc004ee97e84a3e439777eae666e2
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-01-16 18:23:12 +0000

    Tidy up scratchtest_zed.

--- tests/scratch/zed/main.cpp
+++ tests/scratch/zed/main.cpp
@@ -9,8 +9,7 @@
 
 //#################### TYPEDEFS ####################
 
-namespace sl
-{
+namespace sl {
 
 typedef boost::shared_ptr<Camera> Camera_Ptr;
 typedef boost::shared_ptr<const Camera> Camera_CPtr;
@@ -47,7 +46,7 @@ sl::Camera_Ptr init_zed()
 }
 
 /**
- * Conversion function between sl::Mat and cv::Mat
+ * Conversion function between sl::Mat and cv::Mat.
  */
 cv::Mat slMat2cvMat(sl::Mat& input)
 {
@@ -67,7 +66,7 @@ cv::Mat slMat2cvMat(sl::Mat& input)
   }
 
   // Since cv::Mat data requires a uchar* pointer, we get the uchar1 pointer from sl::Mat (getPtr<T>())
-  // cv::Mat and sl::Mat will share a single memory structure
+  // cv::Mat and sl::Mat will share a single memory structure.
   return cv::Mat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(sl::MEM_CPU));
 }
 
@@ -123,103 +122,108 @@ using namespace sl;
 cv::Mat slMat2cvMat(Mat& input);
 void printHelp();
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+  // Create a ZED camera object
+  Camera zed;
 
-    // Create a ZED camera object
-    Camera zed;
+  // Set configuration parameters
+  InitParameters init_params;
+  init_params.camera_resolution = RESOLUTION_HD1080;
+  init_params.depth_mode = DEPTH_MODE_PERFORMANCE;
+  init_params.coordinate_units = UNIT_METER;
 
-    // Set configuration parameters
-    InitParameters init_params;
-    init_params.camera_resolution = RESOLUTION_HD1080;
-    init_params.depth_mode = DEPTH_MODE_PERFORMANCE;
-    init_params.coordinate_units = UNIT_METER;
-
-    // Open the camera
-    ERROR_CODE err = zed.open(init_params);
-    if (err != SUCCESS) {
-        printf("%s\n", errorCode2str(err).c_str());
-        zed.close();
-        return 1; // Quit if an error occurred
-    }
-
-    // Display help in console
-    printHelp();
-
-    // Set runtime parameters after opening the camera
-    RuntimeParameters runtime_parameters;
-    runtime_parameters.sensing_mode = SENSING_MODE_STANDARD;
-
-    // Prepare new image size to retrieve half-resolution images
-    Resolution image_size = zed.getResolution();
-    int new_width = image_size.width / 2;
-    int new_height = image_size.height / 2;
-
-    // To share data between sl::Mat and cv::Mat, use slMat2cvMat()
-    // Only the headers and pointer to the sl::Mat are copied, not the data itself
-    Mat image_zed(new_width, new_height, MAT_TYPE_8U_C4);
-    cv::Mat image_ocv = slMat2cvMat(image_zed);
-    Mat depth_image_zed(new_width, new_height, MAT_TYPE_8U_C4);
-    cv::Mat depth_image_ocv = slMat2cvMat(depth_image_zed);
-    Mat point_cloud;
-
-    // Loop until 'q' is pressed
-    char key = ' ';
-    while (key != 'q') {
-
-        if (zed.grab(runtime_parameters) == SUCCESS) {
-
-            // Retrieve the left image, depth image in half-resolution
-            zed.retrieveImage(image_zed, VIEW_LEFT, MEM_CPU, new_width, new_height);
-            zed.retrieveImage(depth_image_zed, VIEW_DEPTH, MEM_CPU, new_width, new_height);
-
-            // Retrieve the RGBA point cloud in half-resolution
-            // To learn how to manipulate and display point clouds, see Depth Sensing sample
-            zed.retrieveMeasure(point_cloud, MEASURE_XYZRGBA, MEM_CPU, new_width, new_height);
-
-            // Display image and depth using cv:Mat which share sl:Mat data
-            cv::imshow("Image", image_ocv);
-            cv::imshow("Depth", depth_image_ocv);
-
-            // Handle key event
-            key = cv::waitKey(10);
-        }
-    }
+  // Open the camera
+  ERROR_CODE err = zed.open(init_params);
+  if(err != SUCCESS)
+  {
+    printf("%s\n", errorCode2str(err).c_str());
     zed.close();
-    return 0;
-}
+    return 1; // Quit if an error occurred
+  }
 
-/**
-* Conversion function between sl::Mat and cv::Mat
-**/
-cv::Mat slMat2cvMat(Mat& input) {
-    // Mapping between MAT_TYPE and CV_TYPE
-    int cv_type = -1;
-    switch (input.getDataType()) {
-        case MAT_TYPE_32F_C1: cv_type = CV_32FC1; break;
-        case MAT_TYPE_32F_C2: cv_type = CV_32FC2; break;
-        case MAT_TYPE_32F_C3: cv_type = CV_32FC3; break;
-        case MAT_TYPE_32F_C4: cv_type = CV_32FC4; break;
-        case MAT_TYPE_8U_C1: cv_type = CV_8UC1; break;
-        case MAT_TYPE_8U_C2: cv_type = CV_8UC2; break;
-        case MAT_TYPE_8U_C3: cv_type = CV_8UC3; break;
-        case MAT_TYPE_8U_C4: cv_type = CV_8UC4; break;
-        default: break;
+  // Display help in console
+  printHelp();
+
+  // Set runtime parameters after opening the camera
+  RuntimeParameters runtime_parameters;
+  runtime_parameters.sensing_mode = SENSING_MODE_STANDARD;
+
+  // Prepare new image size to retrieve half-resolution images
+  Resolution image_size = zed.getResolution();
+  int new_width = image_size.width / 2;
+  int new_height = image_size.height / 2;
+
+  // To share data between sl::Mat and cv::Mat, use slMat2cvMat()
+  // Only the headers and pointer to the sl::Mat are copied, not the data itself
+  Mat image_zed(new_width, new_height, MAT_TYPE_8U_C4);
+  cv::Mat image_ocv = slMat2cvMat(image_zed);
+  Mat depth_image_zed(new_width, new_height, MAT_TYPE_8U_C4);
+  cv::Mat depth_image_ocv = slMat2cvMat(depth_image_zed);
+  Mat point_cloud;
+
+  // Loop until 'q' is pressed
+  char key = ' ';
+  while(key != 'q')
+  {
+    if(zed.grab(runtime_parameters) == SUCCESS)
+    {
+      // Retrieve the left image, depth image in half-resolution
+      zed.retrieveImage(image_zed, VIEW_LEFT, MEM_CPU, new_width, new_height);
+      zed.retrieveImage(depth_image_zed, VIEW_DEPTH, MEM_CPU, new_width, new_height);
+
+      // Retrieve the RGBA point cloud in half-resolution
+      // To learn how to manipulate and display point clouds, see Depth Sensing sample
+      zed.retrieveMeasure(point_cloud, MEASURE_XYZRGBA, MEM_CPU, new_width, new_height);
+
+      // Display image and depth using cv:Mat which share sl:Mat data
+      cv::imshow("Image", image_ocv);
+      cv::imshow("Depth", depth_image_ocv);
+
+      // Handle key event
+      key = cv::waitKey(10);
     }
+  }
 
-    // Since cv::Mat data requires a uchar* pointer, we get the uchar1 pointer from sl::Mat (getPtr<T>())
-    // cv::Mat and sl::Mat will share a single memory structure
-    return cv::Mat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(MEM_CPU));
+  zed.close();
+  return 0;
 }
 
 /**
-* This function displays help in console
-**/
-void printHelp() {
-    std::cout << " Press 's' to save Side by side images" << std::endl;
-    std::cout << " Press 'p' to save Point Cloud" << std::endl;
-    std::cout << " Press 'd' to save Depth image" << std::endl;
-    std::cout << " Press 'm' to switch Point Cloud format" << std::endl;
-    std::cout << " Press 'n' to switch Depth format" << std::endl;
+ * Conversion function between sl::Mat and cv::Mat
+ **/
+cv::Mat slMat2cvMat(Mat& input)
+{
+  // Mapping between MAT_TYPE and CV_TYPE
+  int cv_type = -1;
+  switch (input.getDataType())
+  {
+    case MAT_TYPE_32F_C1: cv_type = CV_32FC1; break;
+    case MAT_TYPE_32F_C2: cv_type = CV_32FC2; break;
+    case MAT_TYPE_32F_C3: cv_type = CV_32FC3; break;
+    case MAT_TYPE_32F_C4: cv_type = CV_32FC4; break;
+    case MAT_TYPE_8U_C1: cv_type = CV_8UC1; break;
+    case MAT_TYPE_8U_C2: cv_type = CV_8UC2; break;
+    case MAT_TYPE_8U_C3: cv_type = CV_8UC3; break;
+    case MAT_TYPE_8U_C4: cv_type = CV_8UC4; break;
+    default: break;
+  }
+
+  // Since cv::Mat data requires a uchar* pointer, we get the uchar1 pointer from sl::Mat (getPtr<T>())
+  // cv::Mat and sl::Mat will share a single memory structure
+  return cv::Mat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(MEM_CPU));
+}
+
+/**
+ * This function displays help in console
+ **/
+void printHelp()
+{
+  std::cout << " Press 's' to save Side by side images" << std::endl;
+  std::cout << " Press 'p' to save Point Cloud" << std::endl;
+  std::cout << " Press 'd' to save Depth image" << std::endl;
+  std::cout << " Press 'm' to switch Point Cloud format" << std::endl;
+  std::cout << " Press 'n' to switch Depth format" << std::endl;
 }
 
 #endif
