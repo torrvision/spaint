6eb641282f46fa3c01c9256c5a19f8f17cab06db
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-05-11 15:57:51 +0100

    Import the instantiations files from scoreforest_grove.

---
+++ modules/grove/src/CUDAInstantiations.cu
@@ -0,0 +1,35 @@
+/**
+ * grove: CUDAInstantiations.cu
+ * Copyright (c) Torr Vision Group, University of Oxford, 2017. All rights reserved.
+ */
+
+#include "clustering/cuda/ExampleClusterer_CUDA.tcu"
+#include "features/cuda/RGBDPatchFeatureCalculator_CUDA.tcu"
+#include "forests/cuda/DecisionForest_CUDA.tcu"
+#include "reservoirs/cuda/ExampleReservoirs_CUDA.tcu"
+#include "scoreforests/Keypoint3DColourCluster.h"
+#include "scoreforests/ScorePrediction.h"
+using namespace boost;
+using namespace ORUtils;
+
+namespace grove {
+
+//#################### CONSTANTS ####################
+
+namespace {
+  static const int FOREST_TREES = 5;
+}
+
+//#################### EXPLICIT INSTANTIATIONS ####################
+
+template class DecisionForest_CUDA<RGBDPatchDescriptor,FOREST_TREES>;
+template class ExampleClusterer_CUDA<Keypoint3DColour, Keypoint3DColourCluster, ScorePrediction::Capacity>;
+template class ExampleReservoirs_CUDA<Keypoint2D>;
+template class ExampleReservoirs_CUDA<Keypoint3DColour>;
+template class RGBDPatchFeatureCalculator_CUDA<Keypoint2D,RGBDPatchDescriptor>;
+template class RGBDPatchFeatureCalculator_CUDA<Keypoint3DColour,RGBDPatchDescriptor>;
+
+template void ExampleReservoirs_CUDA<Keypoint2D>::add_examples_sub(const ExampleImage_CPtr&, const shared_ptr<const Image<VectorX<int,FOREST_TREES> > >&);
+template void ExampleReservoirs_CUDA<Keypoint3DColour>::add_examples_sub(const ExampleImage_CPtr&, const shared_ptr<const Image<VectorX<int,FOREST_TREES> > >&);
+
+}
