9ad35ed165c918a7c49198ed330eae8610e88196
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-06-07 18:02:27 +0100

    Even more tidying.

--- cmake/UseBoost.cmake
+++ cmake/UseBoost.cmake
@@ -2,7 +2,7 @@
 # UseBoost.cmake #
 ##################
 
-SET(Boost_ADDITIONAL_VERSIONS "1.53" "1.53.0" "1.54" "1.54.0" "1.55" "1.55.0" "1.56" "1.56.0" "1.58.0" "1.67.0")
+SET(Boost_ADDITIONAL_VERSIONS "1.53" "1.53.0" "1.54" "1.54.0" "1.55" "1.55.0" "1.56" "1.56.0" "1.58.0")
 SET(BOOST_ROOT "${PROJECT_SOURCE_DIR}/libraries/boost_1_58_0" CACHE FILEPATH "The Boost root directory")
 SET(Boost_USE_STATIC_LIBS ON)
 FIND_PACKAGE(Boost REQUIRED COMPONENTS timer chrono date_time filesystem program_options regex serialization system thread unit_test_framework)
