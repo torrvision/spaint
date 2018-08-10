863f400b72e2000ed3408541a792d83c7e8c68b8
Tommaso Cavallari <tommaso@robots.ox.ac.uk>
2018-08-10 15:44:21 +0100

    fix linking properly

--- cmake/LinkGrove.cmake
+++ cmake/LinkGrove.cmake
@@ -3,5 +3,8 @@
 ###################
 
 IF(BUILD_GROVE)
-  TARGET_LINK_LIBRARIES(${targetname} grove itmx tvgutil ${CUDA_cudadevrt_LIBRARY})
+  TARGET_LINK_LIBRARIES(${targetname} grove itmx orx tvgutil ${CUDA_cudadevrt_LIBRARY})
+
+  INCLUDE(${PROJECT_SOURCE_DIR}/cmake/LinkALGLIB.cmake)
+  INCLUDE(${PROJECT_SOURCE_DIR}/cmake/LinkScoreForests.cmake)
 ENDIF()
