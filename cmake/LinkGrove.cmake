1a90a9f308ecdbe076067c2e25f73ea876b736bd
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-08-14 15:27:25 +0100

    Fix a build error.

--- cmake/LinkGrove.cmake
+++ cmake/LinkGrove.cmake
@@ -6,5 +6,4 @@ IF(BUILD_GROVE)
   TARGET_LINK_LIBRARIES(${targetname} grove itmx orx tvgutil ${CUDA_cudadevrt_LIBRARY})
 
   INCLUDE(${PROJECT_SOURCE_DIR}/cmake/LinkALGLIB.cmake)
-  INCLUDE(${PROJECT_SOURCE_DIR}/cmake/LinkScoreForests.cmake)
 ENDIF()
