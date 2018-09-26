acc9f3f80cfcbf6e46724567478063cc195fb49d
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-09-26 12:08:53 +0100

    Add some comments.

--- modules/itmx/include/itmx/remotemapping/MappingMessage.h
+++ modules/itmx/include/itmx/remotemapping/MappingMessage.h
@@ -20,19 +20,27 @@ class MappingMessage : public tvgutil::Message
   //#################### PROTECTED MEMBER FUNCTIONS ####################
 protected:
   /**
-   * \brief TODO
+   * \brief Reads a 6D pose from the specified byte segment in the message.
+   *
+   * \param segment The byte segment from which to read the pose.
+   * \return        The 6D pose.
    */
   ORUtils::SE3Pose read_pose(const Segment& segment) const;
 
   /**
-   * \brief TODO
+   * \brief Writes a 6D pose into the specified byte segment in the message.
+   *
+   * \param pose    The 6D pose.
+   * \param segment The byte segment into which to write it.
    */
   void write_pose(const ORUtils::SE3Pose& pose, const Segment& segment);
 
   //#################### PROTECTED STATIC MEMBER FUNCTIONS ####################
 protected:
   /**
-   * \brief TODO
+   * \brief Gets the number of bytes needed to store a 6D pose.
+   *
+   * \return  The number of bytes needed to store a 6D pose.
    */
   static size_t bytes_for_pose();
 };
