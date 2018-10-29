35535e3b22617368bc1f5765f33208fed5917da7
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-10-29 10:10:15 +0000

    Import the CMake script changes from scoreforest_grove.

---
+++ cmake/LinkScoreForests.cmake
@@ -0,0 +1,9 @@
+##########################
+# LinkScoreForests.cmake #
+##########################
+
+# ScoreForests needs C++11 support.
+IF(WITH_SCOREFORESTS AND WITH_C++11)
+  TARGET_LINK_LIBRARIES(${targetname} ${ScoreForests_LIBRARIES})
+  INCLUDE(${PROJECT_SOURCE_DIR}/cmake/LinkALGLIB.cmake)
+ENDIF()
