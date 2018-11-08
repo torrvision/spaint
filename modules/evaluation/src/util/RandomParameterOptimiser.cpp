ffcb9d7f9221355851e1cdcc0150b802e45f23ec
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-11-08 15:29:39 +0000

    Move most of the parameter optimisation functionality up into a superclass.

--- modules/evaluation/src/util/RandomParameterOptimiser.cpp
+++ modules/evaluation/src/util/RandomParameterOptimiser.cpp
@@ -5,83 +5,20 @@
 
 #include "util/RandomParameterOptimiser.h"
 
-#include <limits>
-
-#include <boost/assign/list_of.hpp>
-#include <boost/lexical_cast.hpp>
-using boost::assign::list_of;
-using boost::spirit::hold_any;
-
 namespace evaluation {
 
 //#################### CONSTRUCTORS ####################
 
 RandomParameterOptimiser::RandomParameterOptimiser(const CostFunction& costFunction, size_t epochCount, unsigned int seed)
-: m_costFunction(costFunction), m_epochCount(epochCount), m_rng(seed)
+: EpochBasedParameterOptimiser(costFunction, epochCount, seed)
 {}
-
-//#################### PUBLIC MEMBER FUNCTIONS ####################
-
-RandomParameterOptimiser& RandomParameterOptimiser::add_param(const std::string& param, const std::vector<hold_any>& values)
-{
-  m_paramValues.push_back(std::make_pair(param, values));
-  return *this;
-}
-
-ParamSet RandomParameterOptimiser::optimise_for_parameters(float *bestCost) const
-{
-  std::vector<size_t> bestValueIndicesAllTime;
-  float bestCostAllTime = std::numeric_limits<float>::max();
-
-  // For each epoch:
-  for(size_t i = 0; i < m_epochCount; ++i)
-  {
-    // Randomly generate a set of parameter value indices.
-    std::vector<size_t> valueIndices = generate_random_value_indices();
-
-    // Compute the cost associated to the generated parameters.
-    float configurationCost = compute_cost(valueIndices);
-
-    // If the optimised cost is the best we've seen so far, update the best cost and best parameter value indices.
-    if(configurationCost < bestCostAllTime)
-    {
-      bestCostAllTime = configurationCost;
-      bestValueIndicesAllTime = valueIndices;
-    }
-  }
-
-  // Return the best parameters found and (optionally) the corresponding cost.
-  if(bestCost) *bestCost = bestCostAllTime;
-  return make_param_set(bestValueIndicesAllTime);
-}
 
 //#################### PRIVATE MEMBER FUNCTIONS ####################
 
-float RandomParameterOptimiser::compute_cost(const std::vector<size_t>& valueIndices) const
+std::pair<std::vector<size_t>,float> RandomParameterOptimiser::optimise_value_indices(const std::vector<size_t>& initialValueIndices) const
 {
-  return m_costFunction(make_param_set(valueIndices));
-}
-
-std::vector<size_t> RandomParameterOptimiser::generate_random_value_indices() const
-{
-  std::vector<size_t> valueIndices;
-  for(size_t i = 0, paramCount = m_paramValues.size(); i < paramCount; ++i)
-  {
-    valueIndices.push_back(m_rng.generate_int_from_uniform(0, static_cast<int>(m_paramValues[i].second.size()) - 1));
-  }
-  return valueIndices;
-}
-
-ParamSet RandomParameterOptimiser::make_param_set(const std::vector<size_t>& valueIndices) const
-{
-  ParamSet paramSet;
-  for(size_t i = 0; i < m_paramValues.size(); ++i)
-  {
-    std::string param = m_paramValues[i].first;
-    std::string value = boost::lexical_cast<std::string>(m_paramValues[i].second[valueIndices[i]]);
-    paramSet.insert(std::make_pair(param, value));
-  }
-  return paramSet;
+  // Don't perform any actual optimisation, just compute the cost of the initial parameters.
+  return std::make_pair(initialValueIndices, compute_cost(initialValueIndices));
 }
 
 }
